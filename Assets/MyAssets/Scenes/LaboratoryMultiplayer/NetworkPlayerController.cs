4656b604d1b53250f7053a3c40a30041908551e8
Patrick Hipp <patrick.hipp@student.tugraz.at>
2016-11-02 22:35:33 +0100

    Basic player avatar and some tweeks for the network sync rate

--- Assets/MyAssets/Scenes/LaboratoryMultiplayer/NetworkPlayerController.cs
+++ Assets/MyAssets/Scenes/LaboratoryMultiplayer/NetworkPlayerController.cs
@@ -10,6 +10,8 @@ using UnityStandardAssets.Characters.FirstPerson;
 [RequireComponent(typeof(CharacterController))]
 public class NetworkPlayerController : NetworkBehaviour {
 
+    public GameObject avatar_;
+
 	void Start()
     {
         if (!isLocalPlayer)
@@ -19,6 +21,10 @@ public class NetworkPlayerController : NetworkBehaviour {
             GetComponentInChildren<AudioListener>().enabled = false;
             GetComponentInChildren<Camera>().enabled = false;
             GetComponentInChildren<CharacterController>().enabled = false;
+        }
+        else
+        {
+            avatar_.SetActive(false);
         }
     }
 
