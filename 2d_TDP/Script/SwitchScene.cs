370a05a1f24f858450f065119f026a8730ddbc76
Killutaku <jansenberger@student.tugraz.at>
2017-09-20 06:39:17 +0200

    Add files via upload

---
+++ 2d_TDP/Script/SwitchScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScene : MonoBehaviour 
+{
+	public void Switch(string vScene)
+	{
+		Application.LoadLevel (vScene); //change scene
+	}
+}
