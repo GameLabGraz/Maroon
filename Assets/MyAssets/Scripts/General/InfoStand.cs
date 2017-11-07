029e0bd4d106be90ea68afe2d85af44987a9e955
Johannes Kopf <johannes.kopf@student.tugraz.at>
2017-11-08 00:04:06 +0100

    infostand overhaul

---
+++ Assets/MyAssets/Scripts/General/InfoStand.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoStand : MonoBehaviour
+{
+	private Text _text;
+
+	public string Name;
+
+	private void Awake()
+	{
+		var textFields = GetComponentsInChildren<Text>();
+		_text = textFields.First();
+	}
+
+	private void Start()
+	{
+		_text.text = Name;
+	}
+}
---
+++ Assets/MyAssets/Scripts/General/SceneLoadOnTouch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK.UnityEventHelper;
+
+public class SceneLoadOnTouch : MonoBehaviour
+{
+    private VRTK_InteractableObject_UnityEvents _controllerEvents;
+
+    public String SceneName;
+    
+    private void Awake()
+    {
+        _controllerEvents = GetComponent<VRTK_InteractableObject_UnityEvents>();
+    }
+
+    private void Start()
+    {
+        _controllerEvents.OnUse.AddListener((o, e) => SceneManager.LoadScene(SceneName));
+    }
+}
