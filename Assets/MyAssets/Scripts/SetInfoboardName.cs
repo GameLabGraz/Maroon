546dc89580c6b1ff6bceb83aba81fc749cd9f4ab
Daniel J <jansenberger@student.tugraz.at>
2017-11-15 10:36:00 +0100

    including infoboards from VR and bugfixes

---
+++ Assets/MyAssets/Scripts/SetInfoboardName.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetInfoboardName : MonoBehaviour {
+
+    public Text enterText;
+    public string dialogueKey;
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        string dialogue = GamificationManager.instance.l_manager.GetString(dialogueKey);
+        enterText.text = dialogue;
+    }
+}
