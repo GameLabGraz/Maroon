e4e1bb5591ee11cb2f4c2ed4a42229cca9ab1887
Johannes Kopf <johannes.kopf@student.tugraz.at>
2018-01-04 23:52:33 +0100

    added pressable 3D button to infostand

---
+++ Assets/ButtonSceneLoad.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK;
+using VRTK.UnityEventHelper;
+
+public class ButtonSceneLoad : MonoBehaviour
+{
+    private VRTK_Button_UnityEvents _buttonEvents;
+    public string SceneName;
+    
+    private void Start()
+    {
+        _buttonEvents = GetComponent<VRTK_Button_UnityEvents>();
+        if (_buttonEvents == null)
+            _buttonEvents = gameObject.AddComponent<VRTK_Button_UnityEvents>();
+
+        _buttonEvents.OnPushed.AddListener(HandlePush);
+    }
+
+    private void HandlePush(object sender, Control3DEventArgs e)
+    {
+        VRTK_Logger.Info("Pushed");
+
+        SceneManager.LoadScene(SceneName);
+    }
+}
--- Assets/MyAssets/Scripts/General/InfoStand.cs
+++ Assets/MyAssets/Scripts/General/InfoStand.cs
@@ -5,17 +5,21 @@ using UnityEngine.UI;
 public class InfoStand : MonoBehaviour
 {
 	private Text _text;
+	private ButtonSceneLoad _buttonSceneLoad;
 
-	public string Name;
+	public string ShownName;
+	public string SceneName;
 
 	private void Awake()
 	{
 		var textFields = GetComponentsInChildren<Text>();
 		_text = textFields.First();
+		_buttonSceneLoad = GetComponentInChildren<ButtonSceneLoad>();
 	}
 
 	private void Start()
 	{
-		_text.text = Name;
+		_text.text = ShownName;
+		_buttonSceneLoad.SceneName = SceneName;
 	}
 }
