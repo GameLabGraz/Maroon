37a57bcff41730606b9c822576b78ff93d0c0f44
Johannes Kopf <johannes.kopf@student.tugraz.at>
2018-01-05 00:57:38 +0100

    doors fixed

---
+++ Assets/MyAssets/Scripts/General/DoorSceneLoad.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK.UnityEventHelper;
+
+public class DoorSceneLoad : MonoBehaviour
+{
+	private VRTK_InteractableObject_UnityEvents _events;
+
+	public string SceneName;
+	
+	private void Awake()
+	{
+		_events = GetComponentInChildren<VRTK_InteractableObject_UnityEvents>();
+	}
+
+	private void Start()
+	{
+		_events.OnUse.AddListener((o, e)=>SceneManager.LoadScene(SceneName));
+	}
+}
