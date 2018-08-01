81ea5a6fbe066f1a55e1892a2845f574d71651aa
Peter Oberrauner <peter.oberrauner@rarebyte.com>
2018-08-01 13:46:14 +0200

    Set up New Project for the Project Design Remaster

--- Assets/ExampleScenes/WrenchesTutorialAssets/TurnOnMagnetCaller.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class TurnOnMagnetCaller : MonoBehaviour {
-	void OnMouseUp() {
-        GameObject.Find("StopAndStickTrigger").GetComponent<TurnOnMagnet>().OnMouseUp();
-	}
-}
--- Assets/MyAssets/Models/highvoltagesign/readme.txt
+++
@@ -1,6 +0,0 @@
-Hi,
-on my homepage you will find a transparent (.png) high-voltage plate 700x700 px.
-
-thank you.
-Stephan W.
-www.3d-hobby-art.de
