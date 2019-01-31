ad272cac1ff5f187966d6d74968330b64a86c2ab
Michael Stefan Holly <michael.holly@student.tugraz.at>
2019-01-31 16:20:39 +0100

    Maroon-18_Prevent teleportation through walls

---
+++ Assets/Scripts/PlatformControls/VR/VR_TeleportDisableOnWallCollision.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using VRTK;
+
+namespace PlatformControls.VR
+{
+    [RequireComponent(typeof(VRTK_CollisionTracker))]
+    public class VR_TeleportDisableOnWallCollision : MonoBehaviour
+    {
+        private VRTK_CollisionTracker _collisionTracker;
+        private VRTK_Pointer _pointer;
+
+        private void Start()
+        {
+            _collisionTracker = GetComponent<VRTK_CollisionTracker>();
+            _pointer = GetComponent<VRTK_Pointer>();
+        }
+
+        private void OnEnable()
+        {
+            if (_collisionTracker == null)
+                return;
+
+            _collisionTracker.TriggerEnter += DisableTeleport;
+            _collisionTracker.TriggerExit += EnableTeleport;
+        }
+
+        private void OnDisable()
+        {
+            if (_collisionTracker == null)
+                return;
+
+            _collisionTracker.TriggerEnter -= DisableTeleport;
+            _collisionTracker.TriggerExit -= EnableTeleport;
+        }
+
+        private void ToggleTeleportsEnabled(bool value)
+        {
+            if (_pointer)
+                _pointer.enabled = value;
+
+            foreach (var teleporter in VRTK_ObjectCache.registeredTeleporters)
+                teleporter.ToggleTeleportEnabled(value);
+        }
+
+        private void DisableTeleport(object sender, CollisionTrackerEventArgs e)
+        {
+            if (!e.collider.CompareTag("ExcludeTeleport"))
+                return;
+
+            ToggleTeleportsEnabled(false);
+        }
+
+        private void EnableTeleport(object sender, CollisionTrackerEventArgs e)
+        {
+            if (!e.collider.CompareTag("ExcludeTeleport"))
+                return;
+
+            ToggleTeleportsEnabled(true);
+        }
+    }
+}
