bfc5db1417120b15f0b93a7aaa0d48b88cc4e154
Sandra <brettschuh@student.tugraz.at>
2019-08-03 17:14:03 +0200

    incl new files

---
+++ Assets/Scripts/Experiments/CoulombsLaw/MovingArrow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class MovingArrow : MonoBehaviour
+{
+    private PC_ArrowMovement _arrowMovement;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _arrowMovement = transform.parent.GetComponent<PC_ArrowMovement>();
+        Debug.Assert(_arrowMovement != null);
+    }
+
+    private void OnMouseDown()
+    {
+        _arrowMovement.OnChildMouseDown(gameObject);
+    }
+
+    private void OnMouseDrag()
+    {
+        Debug.Log("OnMouseDrag Arrow Moving");
+
+        _arrowMovement.OnChildMouseDrag();
+    }
+
+    private void OnMouseUp()
+    {
+        _arrowMovement.OnChildMouseUp();
+    }
+}
---
+++ Assets/Scripts/PlatformControls/PC/PC_ZoomMovement.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class PC_ZoomMovement : MonoBehaviour, IResetWholeObject
+{
+    [Tooltip("The nearest position possible.")]
+    public Transform nearPosition;
+    [Tooltip("The furthest position possible.")]
+    public Transform farPosition;
+    [Tooltip("A speed multiplier that tells how fast one zooms.")]
+    public float movementSpeed = 20f;
+
+    [Tooltip("The default position to which the object is set once zooming is enabled.")]
+    [Range(0f, 1f)]
+    public float defaultPosition;
+
+    [Header("Other Settings")] 
+    [Tooltip("Tells if zoom via + and - buttons is enabled.")]
+    public bool enableShortcuts = true;
+    [Tooltip("Tells if zooming is currently enabled or not.")]
+    public bool isEnabled = true;
+
+
+    private Vector3 _positionVariable;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isEnabled)
+            SetToDefaultPosition();
+
+        _positionVariable = (farPosition.position - nearPosition.position).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isEnabled) return;
+        
+        var mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKey(KeyCode.Equals) // because of https://answers.unity.com/questions/249339/problem-with-keycodeplus.html
+            || Input.GetKey(KeyCode.Plus) || Input.GetKey(KeyCode.KeypadPlus))
+            mouseScroll -= 0.1f;
+        if (Input.GetKey(KeyCode.Minus) || Input.GetKey(KeyCode.KeypadMinus))
+            mouseScroll += 0.1f;
+
+        var newPos = transform.position + mouseScroll * movementSpeed * _positionVariable;
+        var near = nearPosition.position;
+        var far = farPosition.position;
+        newPos.x = Mathf.Clamp(newPos.x, Mathf.Min(near.x, far.x), Mathf.Max(near.x, far.x));
+        newPos.y = Mathf.Clamp(newPos.y, Mathf.Min(near.y, far.y), Mathf.Max(near.y, far.y));
+        newPos.z = Mathf.Clamp(newPos.z, Mathf.Min(near.z, far.z), Mathf.Max(near.z, far.z));
+
+        transform.position = newPos;
+    }
+
+    public void EnableZooming(bool enabling)
+    {
+        isEnabled = enabling;
+        if(isEnabled) SetToDefaultPosition();
+    }
+
+    private void SetToDefaultPosition()
+    {
+        transform.position = nearPosition.position +
+                             (farPosition.position - nearPosition.position) * defaultPosition;
+    }
+
+    public void ResetObject()
+    {
+        
+    }
+
+    public void ResetWholeObject()
+    {
+        if(isEnabled) SetToDefaultPosition();
+    }
+}
