7094469f099b743bed4b471908fc0ddc2447bde6
Sebastian Resch <sebastian.resch@student.tugraz.at>
2019-12-03 17:14:11 +0100

    Basic ruler fuctionality

---
+++ Assets/Ruler/RulerLine.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RulerLine : MonoBehaviour
+{
+
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.material.mainTextureScale = new Vector2(this.gameObject.transform.lossyScale.x, 1);
+
+        Vector3 temp = this.gameObject.transform.localScale;
+        temp.x += 0.001f;
+        this.gameObject.transform.localScale = temp;
+    }
+}
