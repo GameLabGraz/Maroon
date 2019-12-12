a81899a8b9877f8fb24c3fd8907bd68e83a37a66
Sandra <brettschuh@student.tugraz.at>
2019-12-12 11:27:06 +0100

    tmp

---
+++ Assets/Scripts/Experiments/CoulombsLaw/PC_SelectScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PC_SelectScript : MonoBehaviour
+{
+    // Selected Object
+    //TODO: highlight the object
+    public GameObject highlightObject;
+
+    public UnityEvent<GameObject> selectedComponent;
+
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse down on Select: " + gameObject.name);
+        selectedComponent.Invoke(gameObject);
+    }
+}
