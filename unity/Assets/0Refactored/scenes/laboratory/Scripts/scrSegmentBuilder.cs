00c7b7df4f1b72309668dc1b2262897b2a83bd2c
Sebastian <sebastian.resch@student.tugraz.at>
2020-05-12 19:11:37 +0200

    Maroon-137 [Laboratory] Generated Laboratory

---
+++ unity/Assets/0Refactored/scenes/laboratory/Scripts/scrSegmentBuilder.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class scrSegmentBuilder : MonoBehaviour
+{
+    // #################################################################################################################
+    // Members
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Settings
+
+    // Activates animation
+    public bool animate;
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Previews
+
+    // Experiment previews to be displayed
+    public GameObject[] prefabsExperimentPreview;
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Given Segments
+
+    // Start segment in the scene
+    [SerializeField] private GameObject staticSegementStart;
+
+    // End segment in the scene
+    [SerializeField] private GameObject staticSegmentEnd;
+
+    // Empty game object that holds segments that are created dynamically
+    [SerializeField] private GameObject staticSegmentContainer;
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Prefabs
+
+    // Prefab to be used as a template for a new segment
+    [SerializeField] private GameObject prefabSegment;
+
+    // Prefab to be placed in a scene if no experiment preview is available   
+    [SerializeField] private GameObject prefabEmptyPreview;
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Counters (exclude start and end segment)
+
+    // Number of segments to be generated to hold all experiment previews (excluding start and end)
+    private int numberSegmentsTotal;
+
+    // Number of segments currently in the scene (excluding start and end)
+    private int numberDynamicSegments = 0;
+
+    // #################################################################################################################
+    // Methods
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(this.BuildAllSegments());
+    }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Builds all segments according to the members
+    public IEnumerator BuildAllSegments()
+    {
+        // Calculate number of segments to be generated
+        this.numberSegmentsTotal = Mathf.CeilToInt(this.prefabsExperimentPreview.Length / 2.0F);
+
+        // Hide segment end
+        this.staticSegmentEnd.SetActive(false);
+
+        // Generate segments
+        while(this.numberDynamicSegments < numberSegmentsTotal)
+        {
+            // If 2 or more previews left
+            if((this.prefabsExperimentPreview.Length - (this.numberDynamicSegments * 2)) >= 2)
+            {
+                this.AddSegment(this.prefabsExperimentPreview[this.numberDynamicSegments * 2],
+                                this.prefabsExperimentPreview[this.numberDynamicSegments * 2 + 1],
+                                this.animate);
+            }
+
+            // If one preview left
+            else
+            {
+                this.AddSegment(this.prefabsExperimentPreview[this.numberDynamicSegments * 2],
+                                this.prefabEmptyPreview,
+                                this.animate);                
+            }
+
+            // Wait before adding next segment
+            if(this.animate)
+            {
+                yield return new WaitForSeconds(1);
+            }
+
+            // Update segment counter
+            this.numberDynamicSegments += 1;
+        }
+
+        // Show segment end
+        if(this.animate)
+        {
+            yield return new WaitForSeconds(6);
+        }
+        this.staticSegmentEnd.SetActive(true);
+    }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Adds one segment to the room
+    private void AddSegment(GameObject prefabPreviewLeft, GameObject prefabPreviewRight, bool animate = false)
+    {
+        // Move end segment forward
+        this.staticSegmentEnd.transform.position += this.staticSegmentEnd.transform.forward * 8;
+
+        // Create new segment
+        GameObject new_segment = Instantiate(this.prefabSegment, new Vector3(0, 0, 500), Quaternion.identity);
+        new_segment.transform.SetParent(this.staticSegmentContainer.transform);
+        var segment_script = new_segment.GetComponent<scrLaboratorySegment>();
+
+        // Add previews
+        segment_script.AddPreview(prefabPreviewLeft);
+        segment_script.AddPreview(prefabPreviewRight, true);
+
+        // Move correct position
+        segment_script.setTargetIndexAndForwardTranslation(this.numberDynamicSegments + 1);
+        segment_script.moveToTargetForwardTranslation(animate);
+    }    
+}
---
+++ unity/Assets/0Refactored/scenes/laboratory/prefabs/segments/Scripts/scrLaboratorySegment.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class scrLaboratorySegment : MonoBehaviour
+{
+    // #################################################################################################################
+    // Members
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Settings
+
+    // Length of this segment
+    [SerializeField] private int segmentLength;
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Constants
+
+    // Length of a small segment
+    private const int segmentLengthSmall = 4;
+
+    // length of a large segment
+    private const int segmentLengthLarge = 8;
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Current values
+
+    // Index of the segment after insertion
+    private int targetIndex = 0;
+
+    // Z position of the segment after insertion
+    private int targetForwardTranslation = 0;
+
+    // True if moved to target position
+    private bool movedToTargetForwardTranslation = false;
+
+    // True if currently moving
+    private bool animationActive = false;
+
+    // #################################################################################################################
+    // Methods: Previews in segment
+
+    // Adds a preview to the segment
+    public void AddPreview(GameObject prefabPreview, bool isRight = false)
+    {
+        // Instantiate new preview
+        GameObject new_preview = Instantiate(prefabPreview, this.gameObject.transform.position, Quaternion.identity,
+                                             this.gameObject.transform);
+
+        // Remove editor only stuff
+        new_preview.transform.GetChild(0).gameObject.SetActive(false);
+        
+        // Rotate preview for other side
+        if(isRight)
+        {
+            new_preview.transform.position = this.gameObject.transform.position +
+                                             new Vector3(0, 0, scrLaboratorySegment.segmentLengthLarge);
+            new_preview.transform.rotation = new Quaternion(0, 180, 0, 0);
+        }
+    }
+
+    // #################################################################################################################
+    // Methods: Index, position
+
+    // Sets target index and re-calculates target forward translation
+    public void setTargetIndexAndForwardTranslation(int targetIndex)
+    {
+        // If segment is first segment
+        if (targetIndex <= 0)
+        {
+            // Set index and translation
+            this.targetIndex = 0;
+            this.targetForwardTranslation = 0;
+        }
+
+        // If segment is any other segment
+        else
+        {
+            // Set index
+            this.targetIndex = targetIndex;
+
+            // Origin + first segment +  all previous segments
+            this.targetForwardTranslation = 0 + scrLaboratorySegment.segmentLengthSmall +
+                                            (targetIndex - 1) * scrLaboratorySegment.segmentLengthLarge;
+        }
+
+        // Segment is possibly not at desired location anymore
+        this.movedToTargetForwardTranslation = false;
+    }
+
+    // Moves segment to target location or starts animation to target location
+    public void moveToTargetForwardTranslation(bool animate = false)
+    {
+        // If animation
+        if(animate)
+        {
+            this.animationActive = true;
+            StartCoroutine(this.AnimateTranslation());
+        }
+
+        // If instant jump to position
+        else
+        {
+            this.gameObject.transform.position = Vector3.zero +
+                                                 this.gameObject.transform.forward * this.targetForwardTranslation;
+            this.movedToTargetForwardTranslation = true;
+        }
+    }
+
+    // #################################################################################################################
+    // Methods: Movement
+
+    // Animation coroutine
+    private IEnumerator AnimateTranslation() 
+    {
+        while(true)
+        {
+            // Calculate target and distance
+            Vector3 vec_target = Vector3.zero + this.gameObject.transform.forward * this.targetForwardTranslation;
+            float distance = Vector3.Distance(vec_target, this.gameObject.transform.position);
+
+            // From infinity to 1m
+            if(distance > 1.0F)
+            {
+                // Move closer by current distance per second
+                this.gameObject.transform.position -= this.gameObject.transform.forward * distance * Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+
+            // From 1m to 0.08m
+            else if(distance > 0.08F)
+            {
+                // Move closer by 1 meter per second
+                this.gameObject.transform.position -= this.gameObject.transform.forward * 1 * Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+
+            // If close enough to target location
+            else
+            {
+                this.gameObject.transform.position = vec_target;
+                this.movedToTargetForwardTranslation = true;
+                this.animationActive = false;
+                break;
+            }
+        }
+    }
+}
