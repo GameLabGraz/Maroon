043fd6b52c9aa13a27e820d1655b924a70163a2a
Sebastian <sebastian.resch@student.tugraz.at>
2020-05-15 20:43:11 +0200

    Draft pause menu

---
+++ unity/Assets/0Refactored/reusableGui/PauseMenu/Scripts/scrPauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrPauseMenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    [SerializeField] private GameObject PauseMenuCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.PauseMenuCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(scrPauseMenu.IsPaused)
+            {
+                this.ClosePauseMenu();
+            }
+            else
+            {
+                this.OpenPauseMenu();
+            }
+        }
+    }
+
+    private void OpenPauseMenu()
+    {
+        this.PauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0;
+        scrPauseMenu.IsPaused = true;
+    }
+
+    public void ClosePauseMenu()
+    {
+        this.PauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1;
+        scrPauseMenu.IsPaused = false;
+    }
+}
