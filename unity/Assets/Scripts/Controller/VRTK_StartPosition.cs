83b4e64131e0ecd9fcb7e417f5cd6723cfb583d3
Sandra <brettschuh@student.tugraz.at>
2020-06-22 12:18:43 +0200

    created vr-spawn position on enter scene

---
+++ unity/Assets/Scripts/Controller/VRTK_StartPosition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class VRTK_StartPosition : VRTK_BasicTeleport
+{
+    [SerializeField] private VRTK_SDKManager vrtkManager; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var setup in vrtkManager.setups)
+        {
+            var args = BuildTeleportArgs(setup.actualBoundaries.transform, transform.position, 
+                setup.actualBoundaries.transform.rotation, true);
+            DoTeleport(this, args);
+        }
+    }
+}
