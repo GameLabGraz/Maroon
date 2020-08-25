8b0c652da1fef9367db7e3d8c6599c34cf8bcabf
Daniel <daniel.dohr@student.tugraz.at>
2020-08-25 15:04:01 +0200

    Maroon-233_FixInterferencePlateWhenPaused

--- unity/Assets/0Refactored/scenes/experiments/HuygensPrinciple/scripts/InterferencePlate.cs
+++ unity/Assets/0Refactored/scenes/experiments/HuygensPrinciple/scripts/InterferencePlate.cs
@@ -2,31 +2,37 @@
 
 namespace Maroon.Physics.HuygensPrinciple
 {
-    public class InterferencePlate : MonoBehaviour
+    public class InterferencePlate : PausableObject
     {
         [SerializeField]
         private SlitPlate slitPlate;
 
         private MeshRenderer _meshRenderer;
-
+        private bool _parameterHasChanged;
+        
         private void Start()
         {
             _meshRenderer = GetComponent<MeshRenderer>();
             UpdateParameters();
         }
 
-
-        private void Update()
+        protected override void HandleUpdate()
         {
-            //ToDo: Add OnMovement Event to the SlitPlate script
-            if (slitPlate.transform.hasChanged)
+            if(!slitPlate.transform.hasChanged && !_parameterHasChanged)
             {
-                slitPlate.transform.hasChanged = false;
-                UpdateParameters();
+                return;
             }
+           
+            _parameterHasChanged = false;
+            slitPlate.transform.hasChanged = false;
+            UpdateParameters();   
         }
 
-        public void UpdateParameters()
+        protected override void HandleFixedUpdate()
+        {   
+        }
+
+        private void UpdateParameters()
         {
             if (slitPlate.NumberOfSlits > 1)
             {
@@ -45,6 +51,11 @@ namespace Maroon.Physics.HuygensPrinciple
 
             _meshRenderer.sharedMaterial.SetFloat(Shader.PropertyToID("_DistanceBetweenPlates"), 
                 Vector4.Distance(slitPlate.transform.position, transform.position));
+        }
+
+        public void SetParamterChangeTrue()
+        {
+            _parameterHasChanged = true;
         }
     }
 }
