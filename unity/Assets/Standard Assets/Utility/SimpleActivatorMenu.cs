8066189f78e31795f22a65028794b17dd0a401c3
Michael Stefan Holly <michael.holly@tugraz.at>
2020-09-14 17:26:46 +0200

    Update to Unity 2019.4.10 LTS

--- unity/Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ unity/Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UnityStandardAssets.Utility
 {
@@ -6,7 +7,7 @@ namespace UnityStandardAssets.Utility
     {
         // An incredibly simple menu which, when given references
         // to gameobjects in the scene
-        public GUIText camSwitchButton;
+        public Text camSwitchButton;
         public GameObject[] objects;
 
 
