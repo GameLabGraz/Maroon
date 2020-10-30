48970256fc2dacfe74bbf28edd685e69485e8ede
Novak Lea <lea.novak@student.tugraz.at>
2020-10-30 19:21:12 +0100

    Maoon-153 [Titration] Integrate chemistry experiment titration (#269)

---
+++ unity/Assets/0Refactored/scenes/experiments/TitrationExperiment/scripts/ShowNameAndFluidcolor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowNameAndFluidcolor : MonoBehaviour 
+{
+
+    [SerializeField] private Text label;
+    [SerializeField] private Renderer labelRend;
+    [SerializeField] private MeshRenderer fluidRend;
+
+    private ShowFluid _showFluidScript;
+
+    private void Start () 
+    {
+        _showFluidScript = ShowFluid.Instance;
+        ChangeName(label);
+    }
+    
+    public void ChangeName (Text label) 
+    {
+        labelRend.material.mainTexture = Resources.Load<Texture>("Sprites/" + label.text);
+
+        if (label.text.Equals("HNO3"))
+            fluidRend.material = _showFluidScript.FluidWaterYellow;
+        else
+            fluidRend.material = _showFluidScript.FluidWaterColorless;
+    }
+}
