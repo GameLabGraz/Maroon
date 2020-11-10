cf66c06292c93b099372c5ccfd3cc716148094f5
Michael Stefan Holly <michael.holly@tugraz.at>
2020-11-10 17:46:35 +0100

    Fix remote images in WebGL

--- unity/Assets/Scripts/Assessment/AssessmentContent/AssessmentImage.cs
+++ unity/Assets/Scripts/Assessment/AssessmentContent/AssessmentImage.cs
@@ -1,13 +1,20 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 using Antares.Evaluation.LearningContent;
-using GEAR.Gadgets.RemoteTexture;
 
 namespace Maroon.Assessment.Content
 {
     [RequireComponent(typeof(UnityEngine.UI.Image))]
     public class AssessmentImage : AssessmentContent
     {
+        private UnityEngine.UI.Image _imageComponent;
+
+        private void Start()
+        {
+            _imageComponent = GetComponent<UnityEngine.UI.Image>();
+        }
+
         public override void LoadContent(Node content)
         {
             if (!(content is Antares.Evaluation.LearningContent.Image image))
@@ -16,12 +23,23 @@ namespace Maroon.Assessment.Content
             var imageComponent = GetComponent<UnityEngine.UI.Image>();
             imageComponent.preserveAspect = true;
 
-            _ = LoadImage(imageComponent, image.Uri);
+            StartCoroutine(LoadImage(image.Uri));
         }
 
-        private static async Task LoadImage(UnityEngine.UI.Image imageComponent, string url)
+        private IEnumerator LoadImage(string url)
         {
-            imageComponent.sprite = await RemoteTexture.GetSprite(url);
+            var www = UnityWebRequestTexture.GetTexture(url);
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                var texture2d = ((DownloadHandlerTexture)www.downloadHandler).texture;
+                _imageComponent.sprite = Sprite.Create(texture2d, new Rect(0, 0, texture2d.width, texture2d.height), new Vector2(0f, 0f));
+            }
         }
     }
 }
