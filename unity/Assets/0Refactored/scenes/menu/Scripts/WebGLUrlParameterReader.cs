a0280003333eacf8e94245d8c0845609289fc04a
Sebastian <sebastian.resch@student.tugraz.at>
2020-12-04 12:29:28 +0100

    JS Unity communication

---
+++ unity/Assets/0Refactored/scenes/menu/Scripts/WebGLUrlParameterReader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class WebGLUrlParameterReader : MonoBehaviour
+{
+    // JS implementation at Plugins/WebGLUrlParameterReader.jslib
+    [DllImport("__Internal")]
+    public static extern string GetUrlParameter(string urlParameterName);
+}
