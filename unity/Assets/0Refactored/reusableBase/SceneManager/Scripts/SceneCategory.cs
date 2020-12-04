2e6462748969359faaa3af838e076719d43dfbc5
Sebastian <sebastian.resch@student.tugraz.at>
2020-12-04 16:38:55 +0100

    Custom scene asset, scene manager files

---
+++ unity/Assets/0Refactored/reusableBase/SceneManager/Scripts/SceneCategory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Maroon
+{
+    [Serializable] class SceneCategory
+    {
+        [SerializeField] private string Name;
+        [SerializeField] private Utilities.SceneField[] SceneFields;
+    }
+}
---
+++ unity/Assets/0Refactored/reusableBase/SceneManager/Scripts/SceneManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Maroon
+{
+    class SceneManager : MonoBehaviour
+    {
+        public CustomSceneAsset test;
+
+        [SerializeField] private Maroon.SceneCategory[] SceneCategories;
+
+        //
+        public void Start()
+        {
+            Debug.Log(test.ScenePath);
+            Debug.Log(test.SceneName);
+            Debug.Log(test.IsVirtualRealityScene);
+        }
+
+        public bool LoadScene(bool showLoadingScreen = false)
+        {
+            return true;
+        }
+
+    }
+}
