d2075647d63fdf19f7c5255fc45b6387ae2b862f
Michael Stefan Holly <michael.holly@tugraz.at>
2021-02-18 15:32:34 +0100

    Fix Pause Button

--- unity/Assets/Maroon/reusableGui/NewUI/Scripts/Runtime/SimulationButtons/SimulationStartButton.cs
+++ unity/Assets/Maroon/reusableGui/NewUI/Scripts/Runtime/SimulationButtons/SimulationStartButton.cs
@@ -7,6 +7,8 @@
         {
             if (!SimulationController.Instance) return;
 
+            gameObject.SetActive(!SimulationController.Instance.SimulationRunning);
+
             SimulationController.Instance.OnStart += (sender, args) => { gameObject.SetActive(false); };
 
             SimulationController.Instance.OnStop += (sender, args) => { gameObject.SetActive(true); };
