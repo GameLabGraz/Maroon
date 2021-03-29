7d7629d8f70fd8c0761a377a7604d05019b9176f
Michael Stefan Holly <michael.holly@tugraz.at>
2021-03-29 15:51:45 +0200

    Falling Coil Room

---
+++ unity/Assets/Maroon/MaybeGlobalEntities/questManager/Scripts/QuestChecks/FallingCoilChecks/StartSimulationCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace QuestManager
+{
+    [RequireComponent(typeof(IQuest))]
+    public class StartSimulationCheck : IQuestCheck
+    {
+        private SimulationController _simulationController;
+        private bool _initSimulationRunning;
+
+        protected override void InitCheck()
+        {
+            _simulationController = FindObjectOfType<SimulationController>();
+            if (_simulationController == null) throw new NullReferenceException("There is no SimulationController in the scene.");
+
+            _initSimulationRunning = false;
+        }
+
+        protected override bool CheckCompliance()
+        {
+            return _initSimulationRunning != _simulationController.SimulationRunning &&
+                   _simulationController.SimulationRunning;
+        }
+    }
+}
