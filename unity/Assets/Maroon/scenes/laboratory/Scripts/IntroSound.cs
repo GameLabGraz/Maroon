ecbb04ca767bf7cdf0fb835e760bac87fe360bd3
Michael Stefan Holly <michael.holly@tugraz.at>
2021-04-07 15:34:04 +0200

    Add Intro Sounds

---
+++ unity/Assets/Maroon/scenes/laboratory/Scripts/IntroSound.cs
@@ -0,0 +1,30 @@
+﻿using GEAR.Localization;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class IntroSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip germanIntro;
+    [SerializeField] private AudioClip englishIntro;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = LanguageManager.Instance.CurrentLanguage == SystemLanguage.German ? germanIntro : englishIntro;
+
+        LanguageManager.Instance.OnLanguageChanged.AddListener((language =>
+        {
+            _audioSource.clip = language == SystemLanguage.German ? germanIntro : englishIntro;
+        }));
+
+        PlayIntro();
+    }
+
+    public void PlayIntro()
+    {
+        _audioSource.Play();
+    }
+}
