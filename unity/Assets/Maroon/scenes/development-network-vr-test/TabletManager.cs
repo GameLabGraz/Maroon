d29cab8fab049c3594396543cc13cb2efe6ae6dd
Sebastian <sebastian.resch@student.tugraz.at>
2021-05-28 15:23:36 +0200

    Test scene for network-vr-tablet experiments

---
+++ unity/Assets/Maroon/scenes/development-network-vr-test/TabletManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class TabletManager : MonoBehaviour
+{
+    enum TabletNetType
+    {
+        Host,
+        Client
+    }
+
+    public TabletNetType net_type = null;
+
+    /// <summary>
+    ///     The SceneCategory that is currently active.
+    //      For example if the player is in the physics lab/category, the physics category should be set to active.
+    /// </summary>
+    public TabletManager.TabletNetType Network
+    {
+        get
+        {
+            return this._activeSceneCategory;
+        }
+
+        set
+        {
+            // Only set if it exists in categories array
+            if(System.Array.Exists(this._sceneCategories, element => element == value))
+            {
+                this._activeSceneCategory = value;
+            }
+        }
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
