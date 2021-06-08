8a791aec71dd1348dd3795d31850148b25af53b0
Sebastian <sebastian.resch@student.tugraz.at>
2021-06-08 13:44:35 +0200

    Temp

---
+++ unity/Assets/Maroon/scenes/development-network-vr-test/ArManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ArManager : MonoBehaviour
+{
+    enum ARNetworkType
+    {
+        Host,
+        Client
+    }
+
+    private TabletNetType _CurrentArNetworkType = null;
+
+    /// <summary>
+    ///     
+    /// </summary>
+    public TabletManager.ARNetworkType CurrentArNetworkType
+    {
+        get
+        {
+            return this._CurrentArNetworkType;
+        }
+
+        set
+        {
+            // Only set if not set already
+            if(this._CurrentArNetworkType == null)
+            {
+                this._CurrentArNetworkType = value;
+            }
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
