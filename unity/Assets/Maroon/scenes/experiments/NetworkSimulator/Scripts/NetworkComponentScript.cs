64594630b8f0b5ed74f287c5810594ea11035ba5
Philipp Umschaden <philippumschaden@icloud.com>
2021-08-18 17:02:37 +0200

    Maroon 355 Network Manager Basic Assets. Added simple asset and script

---
+++ unity/Assets/Maroon/scenes/experiments/NetworkSimulator/Scripts/NetworkComponentScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NetworkComponentScript : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        //Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
+        Debug.Log(name + " Game Object Clicked!");
+        if (pointerEventData.clickCount >= 2)
+        {
+            //change to new scene
+            Debug.Log(name + " Scene will be changed soon!!!!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
