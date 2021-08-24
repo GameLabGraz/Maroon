c441c7477fe10c9cba2e5bc647a270ad72e0781d
Philipp Umschaden <philippumschaden@icloud.com>
2021-08-24 17:31:37 +0200

    Maroon-355 fixed script error

--- unity/Assets/Maroon/scenes/experiments/NetworkSimulator/Scripts/NetworkComponentScript.cs
+++ unity/Assets/Maroon/scenes/experiments/NetworkSimulator/Scripts/NetworkComponentScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class NetworkComponentScript : MonoBehaviour, IPointerClickHandler
+public class NetworkComponentScript : MonoBehaviour
 {
     private  SceneSwitcher sceneSwitcher;
 
