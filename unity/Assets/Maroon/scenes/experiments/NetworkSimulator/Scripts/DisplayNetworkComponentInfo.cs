7d44f6b60746dcb23c6015494dbf40be2a9ebf89
Philipp Umschaden <philippumschaden@icloud.com>
2021-08-30 18:16:19 +0200

    Marron-355-Network-Manager added basic compontens

---
+++ unity/Assets/Maroon/scenes/experiments/NetworkSimulator/Scripts/DisplayNetworkComponentInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayNetworkComponentInfo : MonoBehaviour
+{
+    public string ipV4Adresss;
+    public string subnetMask;
+    public string macAdress;
+    public string mtu;
+    public string status;
+    public string description;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
