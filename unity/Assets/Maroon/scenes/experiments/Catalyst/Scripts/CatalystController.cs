b23d214bac8a5c31c38450fbac71f690011cdc53
Sam-13 <samuel.sommer@student.tugraz.at>
2021-10-13 17:49:09 +0200

    rename Reactor to CatalystReactor, add animation to cat material, stubs for entering the reactor with material and starting the experiment

---
+++ unity/Assets/Maroon/scenes/experiments/Catalyst/Scripts/CatalystController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Maroon.scenes.experiments.Catalyst.Scripts
+{
+    public class CatalystController : MonoBehaviour
+    {
+        private void Start()
+        {
+            SimulationController.Instance.OnStart.AddListener(StartSimulation);
+            SimulationController.Instance.OnStop.AddListener(StopSimulation);
+        }
+
+        private void OnDestroy()
+        {
+            SimulationController.Instance.OnStart.RemoveListener(StartSimulation);
+            SimulationController.Instance.OnStop.RemoveListener(StopSimulation);
+        }
+        
+
+        private void StartSimulation()
+        {
+            Debug.Log("Start catalyst simulation");
+        }
+
+        private void StopSimulation()
+        {
+            Debug.Log("Stop catalyst simulation");
+        }
+    }
+}
--- unity/Assets/Maroon/scenes/experiments/Catalyst/Scripts/CatalystMaterial.cs
+++ unity/Assets/Maroon/scenes/experiments/Catalyst/Scripts/CatalystMaterial.cs
@@ -1,9 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Maroon.scenes.experiments.Catalyst.Scripts
 {
-    public class CatalystMaterial : MonoBehaviour
+    public class CatalystMaterial : MonoBehaviour, IResetObject
     {
-    
+        [SerializeField] Animator animator;
+
+        private readonly int _insertCatalystMaterialAnimationTigger = Animator.StringToHash("insertCatMaterialTrigger");
+        private readonly int _forceExitAnimation = Animator.StringToHash("forceExit");
+        private readonly int _resetAnimation = Animator.StringToHash("reset");
+
+        private bool _isInteractable;
+        
+        private void Start()
+        {
+            _isInteractable = true;
+        }
+
+        public void OnMouseDown()
+        {
+            if (SimulationController.Instance.SimulationRunning && _isInteractable)
+            {
+                animator.SetTrigger(_insertCatalystMaterialAnimationTigger);
+                _isInteractable = false;
+            }
+
+        }
+
+        public void ResetObject()
+        {
+            animator.SetTrigger(_resetAnimation);
+            _isInteractable = true;
+        }
     }
 }
