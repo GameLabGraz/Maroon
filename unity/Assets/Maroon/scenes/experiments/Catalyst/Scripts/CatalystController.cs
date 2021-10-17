a079eac3451bd193e4c98938782dd0c501d42ed5
Sam-13 <samuel.sommer@student.tugraz.at>
2021-10-17 11:57:34 +0200

    catalyst surface and spawning on platinum molecules

--- unity/Assets/Maroon/scenes/experiments/Catalyst/Scripts/CatalystController.cs
+++ unity/Assets/Maroon/scenes/experiments/Catalyst/Scripts/CatalystController.cs
@@ -6,16 +6,30 @@ namespace Maroon.scenes.experiments.Catalyst.Scripts
 {
     public class CatalystController : MonoBehaviour
     {
+        [Header("Catalyst specific objects")]
+        [SerializeField] CatalystReactor catalystReactor;
+        [SerializeField] CatalystSurface catalystSurfacePrefab;
+        [SerializeField] Transform catalystSurfaceSpawnTransform;
+
+        [Header("Player specific objects")]
+        [SerializeField] GameObject player;
+        
+        private CatalystSurface _catalystSurface;
+
         private void Start()
         {
             SimulationController.Instance.OnStart.AddListener(StartSimulation);
             SimulationController.Instance.OnStop.AddListener(StopSimulation);
+
+            catalystReactor.OnSpawnCatalystSurface += SpawnCatalystSurfaceObject;
         }
 
         private void OnDestroy()
         {
             SimulationController.Instance.OnStart.RemoveListener(StartSimulation);
             SimulationController.Instance.OnStop.RemoveListener(StopSimulation);
+            
+            catalystReactor.OnSpawnCatalystSurface -= SpawnCatalystSurfaceObject;
         }
         
 
@@ -27,6 +41,11 @@ namespace Maroon.scenes.experiments.Catalyst.Scripts
         private void StopSimulation()
         {
             Debug.Log("Stop catalyst simulation");
+        }
+
+        private void SpawnCatalystSurfaceObject()
+        {
+            _catalystSurface = Instantiate(catalystSurfacePrefab, catalystSurfaceSpawnTransform);
         }
     }
 }
