a551ddc3bc4de1799e3c490ea3e2dbbe10f667a0
Sam-13 <samuel.sommer@student.tugraz.at>
2021-10-17 10:35:54 +0200

    start of molecules, basic script and prefabs

---
+++ unity/Assets/Maroon/scenes/experiments/Catalyst/Scripts/Molecule.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Maroon.scenes.experiments.Catalyst.Scripts
+{
+    public enum MoleculeType
+    {
+        CO,
+        CO2,
+        O2
+    }
+
+    public class Molecule : MonoBehaviour
+    {
+        [SerializeField] MoleculeType type;
+        
+        public MoleculeType Type { get { return type; } }
+    }
+}
