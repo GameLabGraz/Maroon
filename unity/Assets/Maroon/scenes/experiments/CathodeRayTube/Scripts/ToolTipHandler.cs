d34043cefd5f5c1d471b27b484ea4a1190c819ea
Samuel Hennerbichler <s.hennerbichler@student.tugraz.at>
2022-02-02 16:09:26 +0100

    changed main camera view, added part info when hovering over it, added furniture toggle

---
+++ unity/Assets/Maroon/scenes/experiments/CathodeRayTube/Scripts/ToolTipHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTipHandler : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private GameObject toolTipPanel;
+    [SerializeField] private GameObject toolTipText;
+
+    private void Start()
+    {
+        toolTipPanel.SetActive(false);
+        toolTipText.GetComponent<TextMeshProUGUI>().text = "";
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        
+        toolTipPanel.SetActive(false);
+        toolTipText.GetComponent<TextMeshProUGUI>().text = "";
+        
+        if (Physics.Raycast(ray, out hit) && hit.transform.IsChildOf(transform))
+        {
+            toolTipPanel.SetActive(true);
+            toolTipText.GetComponent<TextMeshProUGUI>().text = hit.transform.name;
+        }
+    }
+}
