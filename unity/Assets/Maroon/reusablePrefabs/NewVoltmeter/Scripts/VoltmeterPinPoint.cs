e4f65ede0e7c1802715f6021d9099a87f77bd6e2
dpdsw <dohr.daniel@gmx.at>
2022-02-28 10:18:18 +0100

    Added new Voltmeter

---
+++ unity/Assets/Maroon/reusablePrefabs/NewVoltmeter/Scripts/VoltmeterPinPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Maroon.Tools.Voltmeter
+{
+    [Serializable] public class VoltmeterEvent : UnityEvent<string> { }
+    public class VoltmeterPinPoint : MonoBehaviour, IResetObject
+    {
+        [SerializeField] private IField eField;
+        [SerializeField] private float potential;
+
+        public VoltmeterEvent onVoltageChanged;
+        public VoltmeterEvent onVoltageChangedUnit;
+
+        public float GetPotential => potential;
+
+        void Update()
+        {
+            if (!gameObject.activeSelf || !eField) return; 
+
+            potential = eField.getStrength(transform.position);
+        }
+
+        public void ResetObject()
+        {
+            gameObject.transform.localRotation = Quaternion.identity;
+            gameObject.SetActive(false);
+        }
+    }
+}
+
---
+++ unity/Assets/Maroon/reusablePrefabs/NewVoltmeter/Scripts/VoltmeterProducers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoltmeterProducers : MonoBehaviour
+{
+    public List<GameObject> producers = new List<GameObject>();
+
+
+    private void Start()
+    {
+        
+    }
+}
