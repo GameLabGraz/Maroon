a5d37e6015f6d7347e6e4cbcd7a28df57e0dc69b
Florian <f.marcher@student.tugraz.at>
2022-03-08 14:55:42 +0100

    Maroon-396_working_noise_mesh
    finished mesh generation, added simple parameters

--- unity/Assets/Maroon/scenes/experiments/PerlinNoise/Scripts/PerlinNoiseExperiment.cs
+++ unity/Assets/Maroon/scenes/experiments/PerlinNoise/Scripts/PerlinNoiseExperiment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -7,14 +9,23 @@ namespace Maroon.scenes.experiments.PerlinNoise.Scripts
 {
     public class PerlinNoiseExperiment : MonoBehaviour
     {
+        [SerializeField, Range(3, 40)] int size = 10;
+        [SerializeField, Range(0, 1)] float scale = 1;
+        [SerializeField, Range(0, 5)] float height_scale = 1;
+        [SerializeField, Range(0, 1)] float thickness = 1;
+        [SerializeField, Range(1, 10)] int octaves = 2;
 
-        [SerializeField] int size = 10;
-        [SerializeField] int scale = 1;
-        [SerializeField] int height_scale = 1;
-        [SerializeField] private MeshFilter meshFilter;
+        [Space(10)] [SerializeField, Range(0, 2)]
+        float speed = 1;
+
+        [SerializeField, Range(0, 20)] private float rotation_speed = 0;
+        [SerializeField] private bool refresh;
+        private MeshFilter meshFilter;
         private Mesh mesh;
         private List<Vector3> vertices;
         private Vector2 offset;
+        private float time;
+        private float rotation;
 
         // Start is called before the first frame update
         void Start()
@@ -25,14 +36,64 @@ namespace Maroon.scenes.experiments.PerlinNoise.Scripts
         // Update is called once per frame
         void Update()
         {
-        
+            rotation += Time.deltaTime * rotation_speed;
+            meshFilter.transform.localRotation = Quaternion.Euler(Vector3.up * rotation);
+
+            if (speed == 0)
+                return;
+
+            time += Time.deltaTime * speed;
+
+            UpdateMesh();
+        }
+
+        private void UpdateMesh()
+        {
+            var index = size * size;
             for (int x = 0; x < size; x++)
             {
                 for (int y = 0; y < size; y++)
                 {
                     vertices[x * size + y] = GetVertexNoise(x, y);
+                    vertices[x * size + y + index] = vertices[x * size + y] + Vector3.down * thickness;
                 }
             }
+
+            index *= 2;
+            for (int x = 0; x < size; x++) //left skirt
+            {
+                var noise = GetVertexNoise(x, 0);
+                vertices[index + 2 * x] = noise + Vector3.down * thickness;
+                vertices[index + 2 * x + 1] = noise;
+            }
+
+            index += size * 2;
+            for (int y = 0; y < size; y++) //bottom skirt
+            {
+                var noise = GetVertexNoise(size - 1, y);
+                vertices[index + 2 * y] = noise + Vector3.down * thickness;
+                vertices[index + 2 * y + 1] = noise;
+            }
+
+            index += size * 2;
+            for (int x = 0; x < size; x++) //right skirt
+            {
+                var noise = GetVertexNoise(x, size - 1);
+                vertices[index + 2 * x] = noise + Vector3.down * thickness;
+                vertices[index + 2 * x + 1] = noise;
+            }
+
+            index += size * 2;
+            for (int y = 0; y < size; y++) //top skirt
+            {
+                var noise = GetVertexNoise(0, y);
+                vertices[index + 2 * y] = noise + Vector3.down * thickness;
+                vertices[index + 2 * y + 1] = noise;
+            }
+
+            if (!mesh)
+                return;
+
             mesh.vertices = vertices.ToArray();
             mesh.RecalculateNormals();
         }
@@ -40,25 +101,24 @@ namespace Maroon.scenes.experiments.PerlinNoise.Scripts
 
         private void GenerateMesh()
         {
-            
             mesh = meshFilter.sharedMesh;
             offset = new Vector2(Random.value, Random.value) * 1e2f;
-            
-            vertices = new List<Vector3>(size * size);
-            var UVs = new List<Vector2>();
+
+            var vertex_count = size * size * 2 + size * 8;
+            vertices = new List<Vector3>(vertex_count);
+            var UVs = new List<Vector2>(vertex_count);
             var indices = new List<int>();
-            
+
             for (int x = 0; x < size; x++)
             {
                 for (int y = 0; y < size; y++)
                 {
-                    
                     vertices.Add(GetVertexNoise(x, y));
                     UVs.Add(new Vector2(x, y));
-                
-                    if(x == 0 || y == 0)
+
+                    if (x == 0 || y == 0)
                         continue;
-                
+
                     indices.AddRange(new[]
                     {
                         vertices.Count - 1, vertices.Count - 2, vertices.Count - 1 - size,
@@ -66,27 +126,154 @@ namespace Maroon.scenes.experiments.PerlinNoise.Scripts
                     });
                 }
             }
-            
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    vertices.Add(vertices[x * size + y] + Vector3.down * thickness);
+                    UVs.Add(new Vector2(x, y));
+
+                    if (x == 0 || y == 0)
+                        continue;
+
+                    indices.AddRange(new[]
+                    {
+                        vertices.Count - 1, vertices.Count - 1 - size, vertices.Count - 2,
+                        vertices.Count - 2 - size, vertices.Count - 2, vertices.Count - 1 - size
+                    });
+                }
+            }
+
+            for (int x = 0; x < size; x++) //left skirt
+            {
+                var noise = GetVertexNoise(x, 0);
+                vertices.Add(noise + Vector3.down * thickness);
+                vertices.Add(noise);
+                UVs.Add(new Vector2(x, -1));
+                UVs.Add(new Vector2(x, 0));
+
+                if (x == 0)
+                    continue;
+
+                indices.AddRange(new[]
+                {
+                    vertices.Count - 1, vertices.Count - 2, vertices.Count - 3,
+                    vertices.Count - 4, vertices.Count - 3, vertices.Count - 2
+                });
+            }
+
+            for (int y = 0; y < size; y++) //bottom skirt
+            {
+                var noise = GetVertexNoise(size - 1, y);
+                vertices.Add(noise + Vector3.down * thickness);
+                vertices.Add(noise);
+                UVs.Add(new Vector2(size, y));
+                UVs.Add(new Vector2(size - 1, y));
+
+                if (y == 0)
+                    continue;
+
+                indices.AddRange(new[]
+                {
+                    vertices.Count - 1, vertices.Count - 2, vertices.Count - 3,
+                    vertices.Count - 4, vertices.Count - 3, vertices.Count - 2
+                });
+            }
+
+            for (int x = 0; x < size; x++) //right skirt
+            {
+                var noise = GetVertexNoise(x, size - 1);
+                vertices.Add(noise + Vector3.down * thickness);
+                vertices.Add(noise);
+                UVs.Add(new Vector2(x, size));
+                UVs.Add(new Vector2(x, size - 1));
+
+                if (x == 0)
+                    continue;
+
+                indices.AddRange(new[]
+                {
+                    vertices.Count - 1, vertices.Count - 3, vertices.Count - 2,
+                    vertices.Count - 4, vertices.Count - 2, vertices.Count - 3
+                });
+            }
+
+            for (int y = 0; y < size; y++) //top skirt
+            {
+                var noise = GetVertexNoise(0, y);
+                vertices.Add(noise + Vector3.down * thickness);
+                vertices.Add(noise);
+                UVs.Add(new Vector2(-1, y));
+                UVs.Add(new Vector2(0, y));
+
+                if (y == 0)
+                    continue;
+
+                indices.AddRange(new[]
+                {
+                    vertices.Count - 1, vertices.Count - 3, vertices.Count - 2,
+                    vertices.Count - 4, vertices.Count - 2, vertices.Count - 3
+                });
+            }
+
+            if (!mesh)
+                return;
+
             mesh.vertices = vertices.ToArray();
             mesh.uv = UVs.ToArray();
             mesh.triangles = indices.ToArray();
-            
-            
-        
             mesh.RecalculateNormals();
         }
 
-        private Vector3 GetVertexNoise(int x, int y)
+        private Vector3 GetVertexNoise(float x, float y)
         {
-            var pos = (offset + new Vector2(x, y)) * scale;
-            var height = Mathf.PerlinNoise(pos.x, pos.y) * height_scale;
-            var pos_offset = size * -0.5f;
-            return new Vector3(x + pos_offset, height, y + pos_offset);
+            var coordinates = new Vector2(x, y);
+            var pos = (offset + coordinates) * scale;
+            var height = PerlinNoise3D(pos.x, pos.y, time);
+            height -= 0.5f;
+            height *= height_scale;
+            coordinates += Vector2.one * (size - 1) * -0.5f;
+            coordinates /= (size - 1);
+            return new Vector3(coordinates.x, height, coordinates.y);
+        }
+
+        public float PerlinNoise3D(float x, float y, float z)
+        {
+            var xy = PerlinNoise2D(x, y);
+            var xz = PerlinNoise2D(x, z);
+            var yz = PerlinNoise2D(y, z);
+            var yx = PerlinNoise2D(y, x);
+            var zx = PerlinNoise2D(z, x);
+            var zy = PerlinNoise2D(z, y);
+
+            return (xy + xz + yz + yx + zx + zy) / 6;
+        }
+
+        public float PerlinNoise2D(float x, float y)
+        {
+            return Mathf.PerlinNoise(x, y);
+            var noise = 0f;
+            for (int i = 1; i <= octaves; i++)
+                noise += (Mathf.PerlinNoise(x * i, y * i) - 0.5f) / i;
+            return noise;
         }
 
         private void OnValidate()
         {
+            refresh = false;
+            if (!meshFilter)
+                meshFilter = GetComponentInChildren<MeshFilter>();
+            if (!meshFilter)
+                return;
+
+            mesh = meshFilter.sharedMesh;
+            var vertex_count = size * size * 2 + size * 8;
+            if (!mesh || mesh.vertexCount == vertex_count)
+                return;
             GenerateMesh();
+            if (!EditorApplication.update.GetInvocationList().Contains((Action) Update))
+                EditorApplication.update += Update;
         }
     }
 }
