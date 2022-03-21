fd72c2d06828f7e492a7db99a33f12e7df5fc131
Sandra Brettschuh <sandra.brettschuh@gmail.com>
2022-03-21 18:55:59 +0100

    more customizations + bugfixes

---
+++ unity/Assets/Brick Project Studio/SE Basic Assets/Scripts and Animations/Doors/opencloseDoor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SojaExiles
+
+{
+	public class opencloseDoor : MonoBehaviour
+	{
+
+		public Animator openandclose;
+		public bool open;
+		public Transform Player;
+
+		void Start()
+		{
+			open = false;
+		}
+
+		void OnMouseOver()
+		{
+			{
+				if (Player)
+				{
+					float dist = Vector3.Distance(Player.position, transform.position);
+					if (dist < 15)
+					{
+						if (open == false)
+						{
+							if (Input.GetMouseButtonDown(0))
+							{
+								StartCoroutine(opening());
+							}
+						}
+						else
+						{
+							if (open == true)
+							{
+								if (Input.GetMouseButtonDown(0))
+								{
+									StartCoroutine(closing());
+								}
+							}
+
+						}
+
+					}
+				}
+
+			}
+
+		}
+
+		IEnumerator opening()
+		{
+			print("you are opening the door");
+			openandclose.Play("Opening");
+			open = true;
+			yield return new WaitForSeconds(.5f);
+		}
+
+		IEnumerator closing()
+		{
+			print("you are closing the door");
+			openandclose.Play("Closing");
+			open = false;
+			yield return new WaitForSeconds(.5f);
+		}
+
+
+	}
+}
