0d02b0488f53aa472aca1c0db9d09b21c367721f
Sandra Brettschuh <sandra.brettschuh@gmail.com>
2022-03-21 18:55:59 +0100

    more customizations + bugfixes

---
+++ unity/Assets/Brick Project Studio/SE Basic Assets/Scripts and Animations/Drawer/X Axis/Drawer_Pull_X.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SojaExiles
+
+{
+
+	public class Drawer_Pull_X : MonoBehaviour
+	{
+
+		public Animator pull_01;
+		public bool open;
+		public Transform Player;
+
+		void Start()
+		{
+			open = false;
+		}
+
+		void OnMouseOver()
+		{
+			{
+				if (Player)
+				{
+					float dist = Vector3.Distance(Player.position, transform.position);
+					if (dist < 10)
+					{
+						print("object name");
+						if (open == false)
+						{
+							if (Input.GetMouseButtonDown(0))
+							{
+								StartCoroutine(opening());
+							}
+						}
+						else
+						{
+							if (open == true)
+							{
+								if (Input.GetMouseButtonDown(0))
+								{
+									StartCoroutine(closing());
+								}
+							}
+
+						}
+
+					}
+				}
+
+			}
+
+		}
+
+		IEnumerator opening()
+		{
+			print("you are opening the door");
+			pull_01.Play("openpull_01");
+			open = true;
+			yield return new WaitForSeconds(.5f);
+		}
+
+		IEnumerator closing()
+		{
+			print("you are closing the door");
+			pull_01.Play("closepush_01");
+			open = false;
+			yield return new WaitForSeconds(.5f);
+		}
+
+
+	}
+}
