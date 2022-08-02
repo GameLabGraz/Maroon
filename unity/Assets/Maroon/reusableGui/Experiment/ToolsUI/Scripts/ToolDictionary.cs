07e1f9081c468db23b4738ae3c713ce82b3dbf79
dpdsw <dohr.daniel@gmx.at>
2022-08-02 11:08:13 +0200

    Initial Commit

---
+++ unity/Assets/Maroon/reusableGui/Experiment/ToolsUI/Scripts/ToolDictionary.cs
@@ -0,0 +1,32 @@
+﻿using GEAR.Gadgets.Attribute;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Maroon.Tools
+{
+    public class ToolDictionary : MonoBehaviour
+    {
+        private List<GameObject> childGameObjects = new List<GameObject>();
+           
+        private void Start()
+        {
+            foreach(Transform child in gameObject.transform)
+            { 
+                childGameObjects.Add(child.gameObject);
+            }
+        }
+
+        public void HideAllOtherTools(GameObject toolToRemainActive)
+        { 
+            foreach(var tool in childGameObjects)
+            {
+                if(toolToRemainActive != tool)
+                    tool.SetActive(false);
+            }
+
+            toolToRemainActive.SetActive(true);
+        }
+    }
+}
