90b1a5b4c989347585a2671a3e6855eea50d87a7
Sam-13 <samuel.sommer@student.tugraz.at>
2022-08-06 12:08:28 +0200

    add increase temp & pressure quests, adjust quest manager colliders to show more quests

---
+++ unity/Assets/Maroon/MaybeGlobalEntities/questManager/Scripts/QuestChecks/Catalyst/IncreasePressure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Maroon.Chemistry.Catalyst;
+using UnityEngine;
+
+namespace QuestManager
+{
+    [RequireComponent(typeof(IQuest))]
+    public class IncreasePressure : IQuestCheck
+    {
+        private CatalystController _catalystController;
+        protected override void InitCheck()
+        {
+            _catalystController = FindObjectOfType<CatalystController>();
+            if (!_catalystController)
+                throw new NullReferenceException("no catalyst controller in scene!");
+        }
+
+        protected override bool CheckCompliance()
+        {
+            return _catalystController.HasInitialPressureChanged;
+        }
+
+    }
+}
