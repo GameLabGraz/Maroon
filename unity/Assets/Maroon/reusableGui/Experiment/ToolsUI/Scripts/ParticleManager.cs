e0a054a260c94730a861d7644e68b26e3419a827
dpdsw <dohr.daniel@gmx.at>
2022-08-12 15:03:55 +0200

    Added lightweight coulombslaw scene

---
+++ unity/Assets/Maroon/reusableGui/Experiment/ToolsUI/Scripts/ParticleManager.cs
@@ -0,0 +1,62 @@
+﻿using Maroon.Physics.Electromagnetism;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    [SerializeField] private GameObject minBoundary;
+    [SerializeField] private GameObject maxBoundary;
+
+    [SerializeField] private EField eField;
+
+    public GameObject CreateSource(GameObject prefab, Vector3 position, float chargeStrength)
+    {
+        var obj = Instantiate(prefab, position, Quaternion.identity);
+        Debug.Assert(obj != null);
+
+        var movement = obj.GetComponent<PC_DragHandler>();
+        if (!movement) movement = obj.GetComponentInChildren<PC_DragHandler>();
+        if (movement)
+        {
+            movement.SetBoundaries(minBoundary, maxBoundary);
+        }
+        
+        obj.GetComponent<Maroon.Physics.Electromagnetism.Charge>().strength = chargeStrength;
+
+        eField.addProducerToSet(obj);
+        eField.updateProducers();
+
+        ChangeColorOfParticle(obj, chargeStrength);
+
+        return obj;
+    }
+
+    public void RemoveSourceFromEField(GameObject source)
+    {
+        eField.removeProducerFromSet(source);
+    }
+
+    public void ChangeColorOfParticle(GameObject obj, float chargeStrength)
+    {
+        var particleBase = obj.transform.Find("Base").GetComponent<MeshRenderer>();
+        var mat = particleBase.materials;
+
+        if (chargeStrength < 0)
+        {
+            mat[0].color = Color.blue;
+            mat[2].color = mat[0].color;
+            mat[1].color = Color.white;
+        }
+        else if (chargeStrength > 0)
+        {
+            mat[0].color = Color.red;
+            mat[1].color = mat[2].color = Color.white;
+        }
+        else
+        {
+            mat[0].color = Color.green;
+            mat[0].color = mat[1].color = mat[2].color;
+        }
+
+        particleBase.materials = mat;
+    }
+}
---
+++ unity/Assets/Maroon/reusableGui/Experiment/ToolsUI/Scripts/UIParticleDragHandler.cs
@@ -0,0 +1,49 @@
+﻿using PlatformControls.PC;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIParticleDragHandler : UIItemDragHandlerSimple, IBeginDragHandler, IEndDragHandler
+{
+    [SerializeField] private ParticleManager _particleManager;
+    [SerializeField] private PC_Slider _chargeValueSlider;
+
+    [Header("Image Components")]
+    [SerializeField] private Image BackgroundImage;
+    [SerializeField] private Image MinusImage;
+    [SerializeField] private Image PlusImage;
+
+    private float _chargeValue = 0.0f;
+
+    public float ChargeValue
+    {
+        get => _chargeValue;
+        set
+        {
+            _chargeValue = value;
+
+            if (Mathf.Abs(_chargeValue) < Mathf.Epsilon)
+            {
+                BackgroundImage.color = Color.green;
+                MinusImage.gameObject.SetActive(false);
+                PlusImage.gameObject.SetActive(false);
+                return;
+            }
+
+            MinusImage.gameObject.SetActive(_chargeValue < 0);
+            PlusImage.gameObject.SetActive(_chargeValue > 0);
+            BackgroundImage.color = _chargeValue < 0 ? Color.blue : Color.red;
+        }
+    }
+
+    private void Start()
+    {
+        ChargeValue = _chargeValueSlider.value;
+        _chargeValueSlider.onValueChanged.AddListener((newValue) => ChargeValue = newValue);
+    }
+
+    protected override void ShowObject(Vector3 position, Transform parent)
+    {
+        var obj = _particleManager.CreateSource(generatedObject, position, ChargeValue);
+    }
+}
