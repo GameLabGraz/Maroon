cc95672bfc0e803b79531d817d1d508ccd9918c7
dpdsw <dohr.daniel@gmx.at>
2022-09-12 08:41:16 +0200

    Added Simulation, Materials

--- unity/Assets/Maroon/reusablePrefabs/NewRuler/Scripts/UIItemDragHandlerUnrestricted.cs
+++ unity/Assets/Maroon/reusablePrefabs/NewRuler/Scripts/UIItemDragHandlerUnrestricted.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+using UnityEngine.Events;
+
 namespace Maroon.Tools.Ruler
 {
     using UnityEngine;
@@ -6,15 +8,22 @@ namespace Maroon.Tools.Ruler
 
     public class UIItemDragHandlerUnrestricted : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
     {
-        public Canvas parentCanvas;
-        public GameObject generatedObject;
-        
-        private GameObject _item;
-        [SerializeField] bool is2D = false;
+        [SerializeField] private Canvas parentCanvas;
+        [SerializeField] private GameObject generatedObject;
 
+        [Header("Boundary Settings")]
+        [SerializeField] private bool isUsingBoundaries;
+        [SerializeField] private Transform minBoundary;
+        [SerializeField] private Transform maxBoundary;
+        [SerializeField] private bool is2D = false;
+
+        private GameObject _item;
+        private bool _objectInsideBoundaries;
+       
         public void OnBeginDrag(PointerEventData eventData)
         {
             _item = Instantiate(gameObject, parentCanvas.transform);
+         
             var recTransformOrig = gameObject.GetComponent<RectTransform>();
             var recTransform = _item.GetComponent<RectTransform>();
 
@@ -25,11 +34,14 @@ namespace Maroon.Tools.Ruler
         public void OnDrag(PointerEventData eventData)
         {
             var screenPoint = Input.mousePosition;
-            var finish = parentCanvas.worldCamera.ScreenToWorldPoint(screenPoint);
+            var finish = screenPoint;
             
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
             if (is2D)
                 finish.z = 0f;
 
+            _objectInsideBoundaries = CheckIfObjectIsInBounds();
             _item.transform.position = finish;
         }
 
@@ -38,20 +50,44 @@ namespace Maroon.Tools.Ruler
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             var hits = Physics.RaycastAll(ray, 1000f);
 
-            foreach (var hit in hits)
+            if (_objectInsideBoundaries)
             {
-                ShowObject(hit.point, hit.transform.parent);
+                foreach (var hit in hits)
+                {
+                    ShowObject(hit.point, hit.transform.parent);
+                }
             }
-
+            
             Destroy(_item);
         }
-
+        
         protected virtual void ShowObject(Vector3 position, Transform parent)
         {
             if (generatedObject == null) return;
 
             generatedObject.transform.position = position;
             generatedObject.SetActive(true);
+        }
+
+        private bool CheckIfObjectIsInBounds()
+        {
+
+            if (maxBoundary != null && minBoundary != null)
+            {
+                var maxBoundPosition = maxBoundary.transform.position;
+                var minBoundPosition = minBoundary.transform.position;
+                var ray = Camera.main.ScreenPointToRay(_item.transform.position);
+                var pt = ray.GetPoint(20);
+
+                if (pt.x >= Mathf.Max(minBoundPosition.x, maxBoundPosition.x) || pt.x <= Mathf.Min(minBoundPosition.x, maxBoundPosition.x))
+                    return false;
+                if (pt.y >= Mathf.Max(minBoundPosition.y, maxBoundPosition.y) || pt.y <= Mathf.Min(minBoundPosition.y, maxBoundPosition.y))
+                    return false;
+                if (!is2D && _item.transform.position.z >= Mathf.Max(minBoundPosition.z, maxBoundPosition.z))
+                    return false;
+            }
+
+            return true;
         }
     }
 }
