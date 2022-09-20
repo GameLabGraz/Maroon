8e818613160744f40181bd9dd2f05fa7b6496138
Marcel Lohfeyer <marcel.lohfeyer@student.tugraz.at>
2022-09-20 18:56:56 +0200

    added assets and textures

---
+++ unity/Assets/Maroon/scenes/experiments/PlanetarySystem/Scripts/SolarSystem.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+    readonly float G = 100f; //G = gravitational constant
+    GameObject[] planets;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planets = GameObject.FindGameObjectsWithTag("Planet");
+
+        InitialVelocity();
+        //InitialVelocityEliptical();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Gravity(); 
+    }
+
+    void Gravity()
+    {    
+        foreach(GameObject a in planets)
+        {
+            foreach(GameObject b in planets)
+            {
+                // object can't orbit itself
+                if(!a.Equals(b))
+                {
+                    float m1 = a.GetComponent<Rigidbody>().mass;
+                    float m2 = b.GetComponent<Rigidbody>().mass;
+
+                    float r = Vector3.Distance(a.transform.position, b.transform.position);
+
+                    // Newton's law of universal gravitation
+                    // F = G * ((m1 * m2) / (r^2))
+                    a.GetComponent<Rigidbody>().AddForce((b.transform.position - a.transform.position).normalized *
+                        (G * (m1 * m2) / (r * r)));
+                }
+            }
+        }
+    }
+
+
+    void InitialVelocity()
+    {
+        foreach(GameObject a in planets)
+        {
+            foreach(GameObject b in planets)
+            {
+                if(!a.Equals(b))
+                {
+                    // m2 = mass of central object
+                    float m2 = b.GetComponent<Rigidbody>().mass;
+                    // r = distance between the objects and a is the length of the semi-mayor axis
+                    float r = Vector3.Distance(a.transform.position, b.transform.position);
+
+                    a.transform.LookAt(b.transform);
+
+                    // circular orbit instant velocity: v0 = sqrt((G * m2) / r)
+                    a.GetComponent<Rigidbody>().velocity += a.transform.right * Mathf.Sqrt((G * m2) / r);
+                }
+            }
+        }
+    }
+
+
+    void InitialVelocityEliptical()
+    {
+        foreach (GameObject a in planets)
+        {
+            foreach (GameObject b in planets)
+            {
+                if (!a.Equals(b))
+                {
+                    // m2 = mass of central object
+                    float m2 = b.GetComponent<Rigidbody>().mass;
+                    // r = distance between the objects
+                    float r = Vector3.Distance(a.transform.position, b.transform.position);
+                    // a = length of the semi-mayor axis
+                    float a_axis = 0f; //TODO
+
+                    a.transform.LookAt(b.transform);
+
+                    // eliptic orbit instant velocity: v0 = G * m2 * (2 / r - 1 / a)
+                    a.GetComponent<Rigidbody>().velocity += a.transform.right * (G * m2 * (2 / r - 1 / a_axis));
+                }
+            }
+        }
+    }
+
+
+
+
+}
