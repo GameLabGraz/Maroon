f2575518c9c0e58f61cb6cb830774c810e6ff4b6
sandra-bre <sandra.brettschuh@gmail.com>
2022-09-20 16:41:59 +0200

    Maroon-419 [HuygensPrinciple VR] Achievements (#422)

---
+++ unity/Assets/Maroon/MaybeGlobalEntities/questManager/Scripts/QuestChecks/HuygensPrinciple/ChangeSlitWidthCheck.cs
@@ -0,0 +1,24 @@
+﻿using Maroon.Physics.HuygensPrinciple;
+using UnityEngine;
+
+namespace QuestManager
+{
+    [RequireComponent(typeof(IQuest))]
+    public class ChangeSlitWidthCheck : IQuestCheck
+    {
+        private SlitPlate _slitPlate;
+        private float _initSlitWidth;
+        private readonly float _targetDifference = 0.1f;
+
+        protected override void InitCheck()
+        {
+            _slitPlate = FindObjectOfType<SlitPlate>();
+            _initSlitWidth = _slitPlate.SlitWidth;
+        }
+
+        protected override bool CheckCompliance()
+        {
+            return Mathf.Abs(_initSlitWidth - _slitPlate.SlitWidth) > _targetDifference;
+        }
+    }
+}
--- unity/Assets/Maroon/reusablePrefabs/Drawer/Scripts/VRScrollingHelper.cs
+++ unity/Assets/Maroon/reusablePrefabs/Drawer/Scripts/VRScrollingHelper.cs
@@ -6,37 +6,22 @@ using UnityEngine;
 public class VRScrollingHelper : MonoBehaviour
 {
     [SerializeField] private VRLinearDrive scrollhandle = null;
+    [SerializeField] private List<GameObject> scrollingOnlyObjects = new List<GameObject>();
     [SerializeField] private List<GameObject> controls = new List<GameObject>();
 
     private const int MaxDisplayObjects = 4;
     private const float LocalZOffset = -0.12f;
     private int _currentPage = 0;
     private bool _firstUpdate = true;
+
+    private int _activeElements = -1;
     
     // Start is called before the first frame update
     void FixedUpdate()
     {
         if (_firstUpdate) //need this otherwise not all StartFunctions of our controls are being called 
         {
-            Debug.Assert(scrollhandle != null);
-
-            if (controls.Count <= MaxDisplayObjects)
-            {
-                //thats the maximum of current displayed controls
-                scrollhandle.gameObject.SetActive(false); // not needed
-            }
-            else
-            {
-                scrollhandle.maximum = controls.Count - MaxDisplayObjects;
-                scrollhandle.stepSize = 1f;
-                scrollhandle.useAsInteger = true;
-
-                scrollhandle.onValueChangedInt.AddListener(x => { UpdateControlsToCurrentPage(x); });
-            }
-
-            _currentPage = 0;
-            UpdateControlsToCurrentPage(_currentPage, true);
-            _firstUpdate = false;
+            RecalculateScrolling(-1);
         }
     }
 
@@ -47,9 +32,11 @@ public class VRScrollingHelper : MonoBehaviour
 
         var isVisible = false;
         var visibleIdx = 0;
+        var activeElementsCnt = GetControlsCount();
+        
         for (var i = 0; i < controls.Count; ++i)
         {
-            isVisible = _currentPage <= i && i < _currentPage + MaxDisplayObjects;
+            isVisible = _currentPage <= i && i < _currentPage + MaxDisplayObjects && i < activeElementsCnt;
             controls[i].SetActive(isVisible);
 
             if (isVisible)
@@ -59,5 +46,46 @@ public class VRScrollingHelper : MonoBehaviour
             }
         }
         Debug.Assert(visibleIdx <= MaxDisplayObjects);
+    }
+
+    public void RecalculateScrolling(int activeElements)
+    {
+        Debug.Assert(scrollhandle != null);
+        _activeElements = activeElements;
+
+        var cntControls = GetControlsCount();
+        if (cntControls <= MaxDisplayObjects)
+        {
+            //thats the maximum of current displayed controls
+            scrollhandle.gameObject.SetActive(false); // not needed
+            foreach(var obj in scrollingOnlyObjects)
+                obj.SetActive(false);
+        }
+        else
+        {
+            scrollhandle.gameObject.SetActive(true);
+            foreach(var obj in scrollingOnlyObjects)
+                obj.SetActive(true);
+            
+            scrollhandle.SetMinMax(0, cntControls - MaxDisplayObjects);
+            scrollhandle.stepSize = 1f;
+            scrollhandle.useAsInteger = true;
+
+            scrollhandle.onValueChangedInt.AddListener(x => { UpdateControlsToCurrentPage(x); });
+        }
+
+        _currentPage = 0;
+        UpdateControlsToCurrentPage(_currentPage, true);
+        _firstUpdate = false;
+    }
+
+    protected int GetControlsCount()
+    {
+        if (_activeElements < 0 || _activeElements > controls.Count)
+        {
+            return controls.Count;
+        }
+
+        return _activeElements;
     }
 }
