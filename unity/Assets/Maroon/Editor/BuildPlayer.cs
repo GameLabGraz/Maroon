3b573dbc4bd7b23600278d6c24268ab36726538d
mfbrantner <martinfbrantner@gmail.com>
2023-02-15 16:44:40 +0100

    i sincerely hope that noone will ever see this

--- unity/Assets/Maroon/Editor/BuildPlayer.cs
+++ unity/Assets/Maroon/Editor/BuildPlayer.cs
@@ -287,8 +287,8 @@ namespace Maroon.Build
             // array of (working) build targets
             // TODO: add WebGL, once it is fixed
             MaroonBuildTarget[] targets = {
-                MaroonBuildTarget.PC
-                MaroonBuildTarget.VR,
+                MaroonBuildTarget.PC,
+                MaroonBuildTarget.VR
             };
             
             // usage: -maroonBuildPath </path/to/build/dir>
