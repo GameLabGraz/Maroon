c879b0c46b8028c6a9ffc14fe0202234577950f9
Jakob Stanta <jakob.stanta@student.tugraz.at>
2023-03-16 16:40:18 +0100

    introduce namespace

--- unity/Assets/Maroon/scenes/experiments/NetworkSimulator/Scripts/DraggableNetworkComponentScript.cs
+++ unity/Assets/Maroon/scenes/experiments/NetworkSimulator/Scripts/DraggableNetworkComponentScript.cs
@@ -1,36 +1,38 @@
 using System.Linq;
 using UnityEngine;
 
-public class DraggableNetworkComponentScript : MonoBehaviour {
-    private Collider collider;
-    private Plane plane;
-    private Vector3 offset;
-    private bool fromKit = true;
-    private Vector3 kitPosition;
+namespace Maroon.NetworkSimulator {
+    public class DraggableNetworkComponentScript : MonoBehaviour {
+        private Collider collider;
+        private Plane plane;
+        private Vector3 offset;
+        private bool fromKit = true;
+        private Vector3 kitPosition;
 
-    void Start() {
-        collider = GetComponent<Collider>();
-        plane = new Plane(Vector3.up, transform.position);
-        kitPosition = transform.position;
-    }
+        void Start() {
+            collider = GetComponent<Collider>();
+            plane = new Plane(Vector3.up, transform.position);
+            kitPosition = transform.position;
+        }
 
-    private void OnMouseDown() {
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        plane.Raycast(ray, out var distance);
-        offset = transform.position - ray.GetPoint(distance);
-    }
+        private void OnMouseDown() {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            plane.Raycast(ray, out var distance);
+            offset = transform.position - ray.GetPoint(distance);
+        }
 
-    void OnMouseDrag() {
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        plane.Raycast(ray, out var distance);
-        transform.position = ray.GetPoint(distance) + offset;
-    }
+        void OnMouseDrag() {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            plane.Raycast(ray, out var distance);
+            transform.position = ray.GetPoint(distance) + offset;
+        }
 
-    private void OnMouseUp() {
-        if(fromKit) {
-            Instantiate(this, kitPosition, Quaternion.identity, transform.parent);
-            transform.parent = FindObjectsOfType<Transform>().Where(t => t.name == "Network").First();
-            fromKit = false;
+        private void OnMouseUp() {
+            if(fromKit) {
+                Instantiate(this, kitPosition, Quaternion.identity, transform.parent);
+                transform.parent = FindObjectsOfType<Transform>().Where(t => t.name == "Network").First();
+                fromKit = false;
+            }
         }
     }
 }
