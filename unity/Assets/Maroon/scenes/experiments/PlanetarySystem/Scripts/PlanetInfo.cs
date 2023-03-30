01536a67135034d46434146a9d74063d12e15849
Marcel Lohfeyer <marcel.lohfeyer@student.tugraz.at>
2023-03-30 20:31:49 +0200

    planetInfo

---
+++ unity/Assets/Maroon/scenes/experiments/PlanetarySystem/Scripts/PlanetInfo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetInfo : MonoBehaviour
+{
+
+    public float mass;
+    public float diameter;
+    public float density;
+    public float gravity;
+    public float escapeVelocity;
+    public float rotationPeriod;
+    public float lengthOfDay;
+    public float semiMajorAxisIDistanceFromSun;
+    public float orbitalPeriod;
+    public float orbitalValocity;
+    public float orbitalInclination;
+    public float orbitalEccentricity;
+    public float obliquityToOrbit;
+    public float meanTemperature;
+
+}
