3e10629360a159c5904428e07c84342b181147da
Marcel Lohfeyer <marcel.lohfeyer@student.tugraz.at>
2023-05-13 16:20:34 +0200

    recreated SortingPlanetSpawner to fix random positions

---
+++ unity/Assets/Maroon/scenes/experiments/PlanetarySystem/Scripts/SortingPlanetSpawner.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingPlanetSpawner : MonoBehaviour
+{
+    public GameObject sortingGamePlanetPlaceholderSlots;
+    private GameObject[] sortingPlanets;
+
+    private List<int> sortingGameAvailableSlotPositions = new List<int>();
+
+
+    /*
+     *
+     */
+    void Start()
+    {
+        Debug.Log("SortingPlanetSpawner: Start(): ");
+
+        //SortingGame
+        Debug.Log("SortingPlanetSpawner: Start(): InitializeAvailableSortingGameSlotPositions()");
+        InitializeAvailableSortingGameSlotPositions();
+        Debug.Log("SortingPlanetSpawner: Start(): SpawnSortingPlanets()");
+        SpawnSortingPlanets();
+    }
+
+
+    /*
+     * handles the SortingGame
+     */
+    #region SortingGameSpawner
+    /*
+     * Initialize the availablePositions list with all possible sorting positions
+     */
+    void InitializeAvailableSortingGameSlotPositions()
+    {
+        int boxSize = 24;
+        for (int i = 0; i < boxSize; i++)
+        {
+            sortingGameAvailableSlotPositions.Add(i);
+        }
+
+        sortingPlanets = GameObject.FindGameObjectsWithTag("SortingGamePlanet");
+        if (sortingPlanets.Length < 1)
+            Debug.Log("SortingPlanetSpawner: InitializeAvailableSortingGameSlotPositions(): no sortingPlanets found");
+    }
+
+
+    /*
+     * Spawn the SortingPlanets on a random sortingGameAvailableSlotPositions
+     */
+    void SpawnSortingPlanets()
+    {
+        for (int i = 0; i < sortingPlanets.Length; i++)
+        {
+            // Randomly select a position index from the sortingGameAvailableSlotPositions list
+            int randomIndex = Random.Range(0, sortingGameAvailableSlotPositions.Count);
+
+            // Get the selected position index and remove it from the list to avoid duplicate positions
+            int positionIndex = sortingGameAvailableSlotPositions[randomIndex];
+            sortingGameAvailableSlotPositions.RemoveAt(randomIndex);
+
+            // Get the child object of the placeholder GameObject at the selected position index
+            Transform spawnPosition = sortingGamePlanetPlaceholderSlots.transform.GetChild(positionIndex);
+            sortingPlanets[i].transform.SetParent(spawnPosition);
+            sortingPlanets[i].transform.position = spawnPosition.position;
+        }
+    }
+    #endregion SortingGameSpawner
+
+}
