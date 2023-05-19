1339c7c83da7223784a6afbc419705fa9096230b
Marcel Lohfeyer <marcel.lohfeyer@student.tugraz.at>
2023-05-19 23:35:15 +0200

    xml; PlanetInfo message and UI

--- unity/Assets/Maroon/scenes/experiments/PlanetarySystem/Scripts/SortingPlanetsDragAndDrop.cs
+++ unity/Assets/Maroon/scenes/experiments/PlanetarySystem/Scripts/SortingPlanetsDragAndDrop.cs
@@ -1,6 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using GEAR.Localization;    //MLG
+using TMPro;
 
 public class SortingPlanetsDragAndDrop : MonoBehaviour, IResetObject
 {
@@ -15,7 +15,14 @@ public class SortingPlanetsDragAndDrop : MonoBehaviour, IResetObject
     public Light sunLightHalo;
     Vector3 mousePosition;
 
+    public TextMeshProUGUI planetInfoMessageText;
+    string createdPlanetInfoMessage;
 
+
+    /*
+     * handle mouse input
+     */
+    #region MouseInput
     /*
      * get mouse position
      */
@@ -25,13 +32,18 @@ public class SortingPlanetsDragAndDrop : MonoBehaviour, IResetObject
         {
             audioSource.PlayOneShot(pickUpClip);
             mousePosition = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+
+            // Show PlanetInfo data when planet is picked up
+            CreatePlanetInfoMessage();
+            planetInfoMessageText.text = createdPlanetInfoMessage;
+
         }
     }
 
 
     /*
-     * calculate position
-     */
+    * calculate position
+    */
     private void OnMouseDrag()
     {
         if (!isSnapped)
@@ -64,12 +76,16 @@ public class SortingPlanetsDragAndDrop : MonoBehaviour, IResetObject
             }
         }
     }
+    #endregion MouseInput
 
 
-
-   /*
-    * increments the snapped planets and displays a Helpi message when all planets except pluto are in its place 
-    */
+    /*
+     * snap the planets to target
+     */
+    #region Snap
+    /*
+     * increments the snapped planets and displays a Helpi message when all planets except pluto are in its place 
+     */
     private void IncrementSnappedPlanetCount()
     {
         int solarSystemPlanet = 10;
@@ -99,13 +115,95 @@ public class SortingPlanetsDragAndDrop : MonoBehaviour, IResetObject
         IncrementSnappedPlanetCount();
         audioSource.PlayOneShot(dropClip);
     }
+    #endregion Snap
 
 
+    /*
+     * CreatePlanetInfoMessage from PlanetInfo for whole PlanetInfoUI
+     */
+    #region PlanetInfoMessage
+    /*
+     * get key from LanguageManager
+     */
+    string GetMessagByKey(string key)
+    {
+        return LanguageManager.Instance.GetString(key);
+    }
+
+
+    /*
+     * CreatePlanetInfoMessage from PlanetInfo for whole PlanetInfoUI
+     */
+    void CreatePlanetInfoMessage()
+    {
+        string planetInfoMessage = "";
+        PlanetInfo planetInfo = GetComponent<PlanetInfo>();
+
+
+        string messageTMP = GetMessagByKey("PlanetInfo0");
+        Debug.Log("SortingPlanetsDragAndDrop: CreatePlanetInfoMessage(): " + messageTMP);
+        //planetInfoMessage += " " + messageTMP + " " + planetInfo.PlanetInformationOf + "\n\n";
+        planetInfoMessage += " " + messageTMP + " " + GetMessagByKey(planetInfo.PlanetInformationOf.ToString()) + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo1");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.mass + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo2");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.diameter + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo3");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.density + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo4");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.gravity + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo5");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.escapeVelocity + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo6");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.rotationPeriod + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo7");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.lengthOfDay + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo8");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.distanceFromSun + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo9");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.perihelion + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo10");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.aphelion + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo11");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.orbitalPeriod + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo12");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.orbitalVelocity + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo13");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.orbitalInclination + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo14");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.orbitalEccentricity + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo15");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.obliquityToOrbit + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo16");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.meanTemperature + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo17");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.surfacePressure + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo18");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.numberOfMoons + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo19");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.ringSystem + "\n\n";
+        messageTMP = GetMessagByKey("PlanetInfo20");
+        planetInfoMessage += " " + messageTMP + " " + planetInfo.globalMagneticField + "\n";
+
+        //combined message
+        createdPlanetInfoMessage = planetInfoMessage;
+    }
+    #endregion PlanetInfoMessage
+
+
+    /*
+     * reset
+     */
+    #region Reset
     /*
      * reset sortingPlanet parent, scale, position, sortedPlanetCount
      */
     public void ResetObject()
     {
+        createdPlanetInfoMessage = "";
+        planetInfoMessageText.text = createdPlanetInfoMessage;
+        //CreatePlanetInfoMessage();
+
         transform.localScale = new Vector3(1, 1, 1);
         PlanetaryController.Instance.ResetSortingGame();
         if (sunLightHalo != null)
@@ -114,4 +212,5 @@ public class SortingPlanetsDragAndDrop : MonoBehaviour, IResetObject
         isSnapped = false;
         PlanetaryController.Instance.sortedPlanetCount = 0;
     }
+    #endregion Reset
 }
