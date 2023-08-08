bb5b685d8b30b8833ffd640ae408b69bc19a450d
Petz <peter.stroissnig@student.tugraz.at>
2023-08-08 20:28:47 +0200

    refactored QuestChecks and removed old QM

---
+++ unity/Assets/Maroon/scenes/experiments/FaradaysLaw/QuestChecks/FieldLineNumberCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using GameLabGraz.QuestManager;
+
+namespace QuestManager
+{
+    [RequireComponent(typeof(Quest))]
+    public class FieldLineNumberCheck : QuestCheck
+    {
+        private SymmetricFieldLineManager _fieldLineManager;
+        private int _initFieldLineNumber;
+
+        protected override void InitCheck()
+        {
+            _fieldLineManager = FindObjectOfType<SymmetricFieldLineManager>();
+            if (_fieldLineManager == null) throw new NullReferenceException("There is no FieldLineManager in the scene.");
+
+            _initFieldLineNumber = _fieldLineManager.GetSymmetryCount();
+        }
+
+        protected override bool CheckCompliance()
+        {
+            return _initFieldLineNumber != _fieldLineManager.GetSymmetryCount();
+        }
+    }
+}
---
+++ unity/Assets/Maroon/scenes/experiments/FaradaysLaw/QuestChecks/VectorFieldResolutionCheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using GameLabGraz.QuestManager;
+
+
+namespace QuestManager
+{
+    [RequireComponent(typeof(Quest))]
+    public class VectorFieldResolutionCheck : QuestCheck
+    {
+        private VectorField _vectorField;
+        private int _initResolution;
+
+        protected override void InitCheck()
+        {
+            _vectorField = FindObjectOfType<VectorField>();
+            if (_vectorField == null) throw new NullReferenceException("There is no VectorField in the scene.");
+
+            _initResolution = _vectorField.Resolution;
+        }
+
+        protected override bool CheckCompliance()
+        {
+            return _initResolution != _vectorField.Resolution;
+        }
+    }
+}
