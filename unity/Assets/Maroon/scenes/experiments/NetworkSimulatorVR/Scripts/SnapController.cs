4bc315133e448b0a9d1931ead724e1b339608e2e
Rijad Kovacevic <rijadinho@gmail.com>
2023-08-21 15:18:46 +0200

    Add asmd

---
+++ unity/Assets/Maroon/scenes/experiments/NetworkSimulatorVR/Scripts/SnapController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapController : MonoBehaviour
+{
+    public List<Transform> snapPoints;
+    public List<BoxCollider> draggable;
+
+    public float snapRange = 0.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
