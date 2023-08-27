b95f50499acf6e9ee9463c3a57ac687216f2c10c
Kovacevic <Rijad.Kovacevic@v2c2.at>
2023-08-27 20:00:08 +0200

    Add addForce to RandomMovement..still some todos

--- unity/Assets/Maroon/scenes/experiments/NetworkSimulatorVR/Scripts/RandomMovement.cs
+++ unity/Assets/Maroon/scenes/experiments/NetworkSimulatorVR/Scripts/RandomMovement.cs
@@ -6,22 +6,42 @@ public class RandomMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
-    [SerializeField] private float speed = 1.0f;
-
+    //[SerializeField] private float speed = 0.1f;
+    private float speed = 0.001f;
+    private float timer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1), ForceMode.Impulse);
- 
+        rb.AddForce(speed, speed, speed, ForceMode.Impulse);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer = timer + Time.deltaTime;
+        Debug.Log("timer: " + timer);
+        if( (timer > 3) && (timer <= 6) )
+        {
+            //rb.transform.position = new Vector3(transform.position.x - speed, transform.position.y - speed, transform.position.z - speed);
+            //timer = 0.0f;
+           
+            rb.AddForce(-speed, -speed, -speed, ForceMode.Impulse);
+        }
+        else
+        {
+            //rb.transform.position = new Vector3(transform.position.x + speed, transform.position.y + speed, transform.position.z + speed);
+            rb.AddForce(speed, speed, speed, ForceMode.Impulse);
+            
+        }
 
-        rb.AddForce(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1), ForceMode.Impulse);
+        if( timer > 6)
+        {
+            timer = 0.0f;
+        }
 
     }
 
