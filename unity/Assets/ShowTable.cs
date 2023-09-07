1325feb6db4eb20a464a022fcb0c213d982012d1
Kovacevic <Rijad.Kovacevic@v2c2.at>
2023-09-07 16:35:13 +0200

    Remove some animations and add on play

---
+++ unity/Assets/ShowTable.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach this script to a GameObject with an Animator component attached.
+//For this example, create parameters in the Animator and name them “Crouch” and “Jump”
+//Apply these parameters to your transitions between states
+
+//This script allows you to trigger an Animator parameter and reset the other that could possibly still be active. Press the up and down arrow keys to do this.
+
+
+public class ShowTable : MonoBehaviour
+{
+    Animator m_Animator;
+
+    void Start()
+    {
+        //Get the Animator attached to the GameObject you are intending to animate.
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        m_Animator.Play(1);
+        //Press the up arrow button to reset the trigger and set another one
+
+    }
+}
