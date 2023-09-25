799cc3775c639bcafec846d16a49d4b50d952228
Kovacevic <Rijad.Kovacevic@v2c2.at>
2023-09-25 11:03:40 +0200

    Fix middle quest

--- unity/Assets/Maroon/MaybeGlobalEntities/questManager/Scripts/QuestChecks/NetworkSimulatorVR/MiddleCorrectlyPlaced.cs
+++ unity/Assets/Maroon/MaybeGlobalEntities/questManager/Scripts/QuestChecks/NetworkSimulatorVR/MiddleCorrectlyPlaced.cs
@@ -6,9 +6,10 @@ namespace QuestManager
     [RequireComponent(typeof(IQuest))]
     public class MiddleCorrectlyPlaced : IQuestCheck
     {
-        private DragObject source;
-        private DragObject destination;
-        private DragObject gateway;
+        private DragObjectMiddle source;
+        private DragObjectMiddle destination;
+        private DragObjectMiddle outgoing;
+        private DragObjectMiddle incoming;
 
         private bool left_completed = false;
 
@@ -16,11 +17,13 @@ namespace QuestManager
         {
             var sourceGameObject = GameObject.Find("SourceM");
             var destinationGameObject = GameObject.Find("DestinationM");
-            var gatewayGameObject = GameObject.Find("GatewayM");
+            var outgoingGameObject = GameObject.Find("Outgoing");
+            var incomingGameObject = GameObject.Find("Incoming");
 
-            source = sourceGameObject.GetComponent<DragObject>();
-            destination = destinationGameObject.GetComponent<DragObject>();
-            gateway = gatewayGameObject.GetComponent<DragObject>();
+            source = sourceGameObject.GetComponent<DragObjectMiddle>();
+            destination = destinationGameObject.GetComponent<DragObjectMiddle>();
+            outgoing = outgoingGameObject.GetComponent<DragObjectMiddle>();
+            incoming = incomingGameObject.GetComponent<DragObjectMiddle>();
 
         }
 
@@ -28,7 +31,8 @@ namespace QuestManager
         {
             if ((source.source_snapped == true) &&
                 (destination.destination_snapped == true) &&
-                (gateway.gateway_snapped == true))
+                (outgoing.position_snapped == true) && 
+                ( incoming.position_snapped == true))
             {
                 left_completed = true;
             }
