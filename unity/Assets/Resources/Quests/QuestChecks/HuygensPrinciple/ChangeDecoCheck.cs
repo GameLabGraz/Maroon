2e7f0a435ff7fbc63dc5018989b90e58be0168c9
Peter <45873995+Petz27@users.noreply.github.com>
2023-10-04 12:55:54 +0200

    Maroon-445 Quest Manager Reimplementation (#449)

---
+++ unity/Assets/Resources/Quests/QuestChecks/HuygensPrinciple/ChangeDecoCheck.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using GameLabGraz.QuestManager;
+
+namespace Quests
+{
+    [RequireComponent(typeof(Quest))]
+    public class ChangeDecoCheck : QuestCheck
+    {
+        class CheckObj
+        {
+            public GameObject _object;
+            public bool _initState;
+
+            public CheckObj(GameObject obj)
+            {
+                _object = obj;
+                _initState = obj.activeInHierarchy;
+            }
+            
+            public bool HasChanged()
+            {
+                return _initState != _object.activeInHierarchy;
+            }
+        }
+
+        private List<CheckObj> _decoObjects = new List<CheckObj>();
+        private bool _wasChanged = false;
+        
+        protected override void InitCheck()
+        {
+            var decoCollection = FindObjectOfType<DecoObjectCollection>();
+            if(decoCollection == null) throw new NullReferenceException("Deco Collection not found.");
+
+            var objs = decoCollection._decoObjects;
+
+            if(objs.Count == 0) throw new NullReferenceException("No deco objects have been found.");
+
+            foreach (var obj in objs)
+            {
+                _decoObjects.Add(new CheckObj(obj));
+            }
+        }
+        
+        protected override bool CheckCompliance()
+        {
+            if (_wasChanged)
+                return true;
+
+            foreach (var obj in _decoObjects)
+            {
+                if (obj.HasChanged())
+                {
+                    _wasChanged = true;
+                    break;
+                }
+            }
+
+            return _wasChanged;
+        }
+    }
+}
