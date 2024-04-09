96df5601a37fa0ba51104719c6a9d981148c3f9e
Daniel Liegl <d.liegl@student.tugraz.at>
2024-04-09 18:23:06 +0200

    Added License for Caliper Sprites

--- unity/Assets/Maroon/scenes/experiments/FallingBallViscosimeter/Scripts/Tools/Caliper/MeasuringTape.cs
+++ unity/Assets/Maroon/scenes/experiments/FallingBallViscosimeter/Scripts/Tools/Caliper/MeasuringTape.cs
@@ -4,21 +4,14 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Caliper : MonoBehaviour
+public class MeasuringTape : MonoBehaviour
 {
-    private Transform slider;
-    private Transform head;
-    private TMP_Text text;
-    private float measuredLength;
     
-    // Start is called before the first frame update
-    void Awake()
-    {
-        text = GetComponentInChildren<TMP_Text>();
-        slider = transform.Find("Slider");
-        head = transform.Find("Head");
-    }
-
+    [SerializeField]private Transform slider;
+    [SerializeField]private Transform head;
+    [SerializeField]private TMP_Text text;
+    private float measuredLength;
+    [SerializeField] private float offset;
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -30,7 +23,7 @@ public class Caliper : MonoBehaviour
         //sets the text to the measured distance
         //floats are dumb so we get some rounding errors but whatever
         //0.0075 is the distance between the slider transform and the head transform
-        measuredLength = (head.position.x - slider.position.x + 0.0075f) * -1;
+        measuredLength = (head.position.y - slider.position.y + offset) * -1;
         text.text = Math.Round(measuredLength, 4).ToString("N4");
     }
 }
