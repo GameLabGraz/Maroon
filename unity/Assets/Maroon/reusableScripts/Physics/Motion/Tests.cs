165737b80ab1e505399f8ef97015f36b7431af1d
Jonathan Platzer <platzerjonathan@gmail.com>
2024-04-19 15:51:52 +0200

    Run test with every implemented Integrator

--- unity/Assets/Maroon/reusableScripts/Physics/Motion/Tests.cs
+++ unity/Assets/Maroon/reusableScripts/Physics/Motion/Tests.cs
@@ -9,11 +9,21 @@ namespace Maroon.Physics.Motion
             var model = new Model("-4*vx", "-4*vy", "-4*vz - m*9.807", "1");
             var state = new State(new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 10.0f), model);
 
-            var solver = new Solver(0.01f, 200);
+            var solver_rk4 = new Solver(0.01f, 200, new RungeKutta4());
+            var solver_ee = new Solver(0.01f, 200, new ExplicitEuler());
+            var solver_sie = new Solver(0.01f, 200, new SemiImplicitEuler());
 
-            solver.AddObject(state);
-            solver.Solve();
-            solver.PrintLog();
+            solver_rk4.AddObject(state);
+            solver_rk4.Solve();
+            solver_rk4.PrintLog();
+
+            solver_ee.AddObject(state);
+            solver_ee.Solve();
+            solver_ee.PrintLog();
+
+            solver_sie.AddObject(state);
+            solver_sie.Solve();
+            solver_sie.PrintLog();
         }
     }
 }
