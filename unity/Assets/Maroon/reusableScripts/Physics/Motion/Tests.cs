8198c169ee389f403b75663af7e1288c355fbfce
Jonathan Platzer <platzerjonathan@gmail.com>
2024-04-19 15:49:13 +0200

    Add tests to own file

---
+++ unity/Assets/Maroon/reusableScripts/Physics/Motion/Tests.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Maroon.Physics.Motion
+{
+    public class Tests
+    {
+        public static void TestSolver()
+        {
+            var model = new Model("-4*vx", "-4*vy", "-4*vz - m*9.807", "1");
+            var state = new State(new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 10.0f), model);
+
+            var solver = new Solver(0.01f, 200);
+
+            solver.AddObject(state);
+            solver.Solve();
+            solver.PrintLog();
+        }
+    }
+}
