113603e4bb0e5ae1d4ca7830378e4c76fd1e1896
Jonathan Platzer <platzerjonathan@gmail.com>
2024-05-06 16:00:46 +0200

    Update Test to use new Simulation API

--- unity/Assets/Maroon/reusableScripts/Physics/Motion/Tests.cs
+++ unity/Assets/Maroon/reusableScripts/Physics/Motion/Tests.cs
@@ -4,26 +4,21 @@ namespace Maroon.Physics.Motion
 {
     public class Tests
     {
-        public static void TestSolver()
+        public static void Test()
         {
-            var model = new Model("-4*vx", "-4*vy", "-4*vz - m*9.807", "1");
-            var state = new State(new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 10.0f), model);
+            var entity = new SimulatedEntity();
+            entity.SetInitialState(new State(new Vector3d(1, 0, 0), new Vector3d(0, 1, 0)));
+            entity.AddExpression("fx", "-x");
+            entity.AddExpression("fy", "-y");
 
-            var solver_rk4 = new Solver(0.01f, 200, new RungeKutta4());
-            var solver_ee = new Solver(0.01f, 200, new ExplicitEuler());
-            var solver_sie = new Solver(0.01f, 200, new SemiImplicitEuler());
+            var sim = new Simulation();
+            sim.dt = 0.1;
+            sim.t0 = 0;
+            sim.steps = 630;
+            sim.AddEntity(entity);
+            sim.Run();
 
-            solver_rk4.AddObject(state);
-            solver_rk4.Solve();
-            solver_rk4.PrintLog();
-
-            solver_ee.AddObject(state);
-            solver_ee.Solve();
-            solver_ee.PrintLog();
-
-            solver_sie.AddObject(state);
-            solver_sie.Solve();
-            solver_sie.PrintLog();
+            entity.PrintDataCsv("");
         }
     }
 }
