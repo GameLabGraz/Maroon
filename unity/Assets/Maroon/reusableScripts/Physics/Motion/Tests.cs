8ada7ed3ac50286ef3b3b43ed2d9448954c48efa
Jonathan Platzer <platzerjonathan@gmail.com>
2024-05-07 15:20:39 +0200

    Add some more tests for the time being

--- unity/Assets/Maroon/reusableScripts/Physics/Motion/Tests.cs
+++ unity/Assets/Maroon/reusableScripts/Physics/Motion/Tests.cs
@@ -6,16 +6,46 @@ namespace Maroon.Physics.Motion
     {
         public static void Test()
         {
+            Circular();
+            RocketStart();
+            B_deflect();
+        }
+
+        public static void Circular()
+        {
+            Debug.Log("Running test Circular");
+
             var entity = new SimulatedEntity();
-            entity.SetInitialState(new State(new Vector3d(0, 0, 0), new Vector3d(0, 0, 0)));
-            entity.AddExpression("fx", "1.6022e-19*(5000/0.05)*H(0.05-x)");
-            entity.AddExpression("fy", "1.6022e-19*(60/0.02)*H(0.1-abs(x-0.25))");
+            entity.SetInitialState(new State(new Vector3d(1, 0, 0), new Vector3d(0, 1, 0)));
+            entity.AddExpression("fx", "-x");
+            entity.AddExpression("fy", "-y");
             entity.AddExpression("fz", "0");
-            entity.AddExpression("m", "9.11e-31");
-            entity.AddExpression("h(x)", "if(x = 0, 0.5, (1+x/abs(x))/2)");
+            entity.AddExpression("m", "1");
 
             var sim = new Simulation();
-            sim.dt = 2.9e-11;
+            sim.dt = 0.01;
+            sim.t0 = 0;
+            sim.steps = 630;
+            sim.AddEntity(entity);
+            sim.Run();
+
+            entity.PrintData();
+        }
+
+        public static void RocketStart()
+        {
+            Debug.Log("Running test RocketStart");
+
+            var entity = new SimulatedEntity();
+            entity.SetInitialState(new State(new Vector3d(0, 0, 0), new Vector3d(0, 0, 0)));
+            entity.AddExpression("fx", "(-0.01*(vx-(0.2*exp(-t*t/50)))-0.03*(vx-(0.2*exp(-t*t/50)))*sqrt((vx-(0.2*exp(-t*t/50)))*(vx-(0.2*exp(-t*t/50)))+(vy-(-0.3*exp(-t*t/50)))*(vy-(-0.3*exp(-t*t/50)))+(vz-(0))*(vz-(0))))");
+            entity.AddExpression("fy", "(-0.01*(vy-(-0.3*exp(-t*t/50)))-0.03*(vy-(-0.3*exp(-t*t/50)))*sqrt((vx-(0.2*exp(-t*t/50)))*(vx-(0.2*exp(-t*t/50)))+(vy-(-0.3*exp(-t*t/50)))*(vy-(-0.3*exp(-t*t/50)))+(vz-(0))*(vz-(0))))");
+            entity.AddExpression("fz", "(-0.01*(vz-(0))-0.03*(vz-(0))*sqrt((vx-(0.2*exp(-t*t/50)))*(vx-(0.2*exp(-t*t/50)))+(vy-(-0.3*exp(-t*t/50)))*(vy-(-0.3*exp(-t*t/50)))+(vz-(0))*(vz-(0))))-9.81*0.1*(2-H(3-t)*t/3-H(t-3))+5*H(3-t)");
+            entity.AddExpression("m", "0.1*(2-H(3-t)*t/3-H(t-3))");
+            entity.AddExpression("h(x)", "if(x == 0, 0.5, (1+x/abs(x))/2)");
+
+            var sim = new Simulation();
+            sim.dt = 0.02;
             sim.t0 = 0;
             sim.steps = 500;
             sim.AddEntity(entity);
@@ -23,5 +53,28 @@ namespace Maroon.Physics.Motion
 
             entity.PrintData();
         }
+
+        public static void B_deflect()
+        {
+            Debug.Log("Running test B_deflect");
+
+            var entity = new SimulatedEntity();
+            entity.SetInitialState(new State(new Vector3d(0, 0, 0), new Vector3d(0, 0, 0)));
+            entity.AddExpression("fx", "1.6022e-19*100000*H(0.05-x)-1.6022e-19*vy*(0.0025)*H(0.03-sqrt((x-0.15)*(x-0.15)+y*y))");
+            entity.AddExpression("fy", "1.6022e-19*vx*(0.0025)*H(0.03-sqrt((x-0.15)*(x-0.15)+y*y))");
+            entity.AddExpression("fz", "0");
+            entity.AddExpression("m", "9.11e-31");
+            entity.AddExpression("h(x)", "if(x == 0, 0.5, (1+x/abs(x))/2)");
+
+            var sim = new Simulation();
+            sim.dt = 2.4E-11;
+            sim.t0 = 0;
+            sim.steps = 500;
+            sim.AddEntity(entity);
+            sim.Run();
+
+            entity.PrintData();
+        }
+
     }
 }
