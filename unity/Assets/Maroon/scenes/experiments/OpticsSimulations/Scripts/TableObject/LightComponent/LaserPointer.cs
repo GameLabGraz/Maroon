230bc7e0a163e48b37932bfa4227ec0044fe032c
Alex <75789482+Xalos11@users.noreply.github.com>
2024-05-08 14:56:29 +0200

    Maroon-455 Optics Simulation Extension (#477)

---
+++ unity/Assets/Maroon/scenes/experiments/OpticsSimulations/Scripts/TableObject/LightComponent/LaserPointer.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using Maroon.Physics.Optics.Light;
+using UnityEngine;
+
+namespace Maroon.Physics.Optics.TableObject.LightComponent
+{
+    public class LaserPointer : LightComponent
+    {
+        private void Start()
+        {
+            LightRoutes = new List<LightPath>();
+            Origin = transform.localPosition;
+
+            foreach (var wl in Wavelengths)
+                LightRoutes.Add(new LightPath(wl));
+            
+            RecalculateLightRoute();
+        }
+        
+        public override void RecalculateLightRoute()
+        {
+            if (LightRoutes == null)
+                return;
+            
+            ResetLightRoutes(1);
+
+            for (int i = 0; i < Wavelengths.Count; i++) // We got Wavelengths.Count many LightRoutes
+            {
+                var initialRay = new RaySegment(Origin, Intensity, Wavelengths[i], transform.right);
+                LightRoutes[i].AddRaySegment(initialRay);
+                LightRoutes[i].CalculateNextRay(initialRay);
+            }
+        }
+        
+    }
+}
