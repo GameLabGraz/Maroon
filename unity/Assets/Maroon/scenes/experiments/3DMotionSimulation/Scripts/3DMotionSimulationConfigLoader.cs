a1c3513388024d6691c777cb2a7177b61b3ae9fb
Florian Wohlmuth <wohlmuth.florian@gmx.at>
2024-07-31 14:55:18 +0200

    Added ConfigLoader for 3DMotionSimulation

---
+++ unity/Assets/Maroon/scenes/experiments/3DMotionSimulation/Scripts/3DMotionSimulationConfigLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Maroon.Config
+{
+    public class ConfigLoader3DMotionSimulation : Maroon.Config.ConfigLoader
+    {
+        public override void LoadConfig()
+        {
+            Debug.Log("Loading 3D Motion Simulation Config");
+        }
+    }
+}
