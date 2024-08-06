5e0386ac688720d480ca26c613e12c424bf0c74d
Florian Wohlmuth <wohlmuth.florian@gmx.at>
2024-08-06 12:13:57 +0200

    Changed Custom ConfigLoader name

--- unity/Assets/Maroon/scenes/experiments/3DMotionSimulation/Scripts/ConfigLoader3DMotionSimulation.cs
+++ unity/Assets/Maroon/scenes/experiments/3DMotionSimulation/Scripts/ConfigLoader3DMotionSimulation.cs
@@ -1,4 +1,3 @@
-#if UNITY_WEBGL && !UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +7,10 @@ namespace Maroon.Config
 {
     public class ConfigLoader3DMotionSimulation : Maroon.Config.ConfigLoader
     {
-        public override void SetParameters()
+        protected override void SetParameters()
         {
             var parameters = JsonConvert.DeserializeObject<ParameterLoader.Parameters>(_parametersString);
             ParameterUI.Instance.LoadParameters(parameters);
         }
     }
 }
-#endif
