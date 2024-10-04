63cd5f72d888e40a0d02ddf13f98e88c223e12f5
Marcel Lohfeyer <marcel.lohfeyer@student.tugraz.at>
2024-10-04 14:45:33 +0200

    added hover effect on screens

--- unity/Assets/Maroon/scenes/experiments/PlanetarySystem/Scripts/StartInteraction.cs
+++ unity/Assets/Maroon/scenes/experiments/PlanetarySystem/Scripts/StartInteraction.cs
@@ -5,14 +5,44 @@ namespace Maroon.Experiments.PlanetarySystem
 {
     public class StartInteraction : MonoBehaviour
     {
+        public UnityEvent onMouseDownEvent;
+        public Color hoverColor;
+
+        // screen png
+        private Material material;
+        private Color originalColor;
+
+        /*
+         * Store the screen png
+         */
+        private void Start()
+        {
+            material = GetComponent<Renderer>().material;
+            originalColor = material.color;
+        }
+
         /*
          * UnityEvent to starts SortingGame or Animation when the screen is clicked
          */
-        public UnityEvent onMouseDownEvent;
-
         private void OnMouseDown()
         {
             onMouseDownEvent.Invoke();
+        }
+
+        /*
+         * Update the material's color on hover
+         */
+        private void OnMouseEnter()
+        {
+            material.color = hoverColor;
+        }
+
+        /*
+         * Restore the original color on mouse exit
+         */
+        private void OnMouseExit()
+        {
+            material.color = originalColor;
         }
     }
 }
