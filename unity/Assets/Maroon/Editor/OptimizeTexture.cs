5421e9ba6b7b31f0655ae51001cf834e53e51251
Florian Glawogger <73254596+FlorianGlawogger@users.noreply.github.com>
2024-10-23 15:10:44 +0200

    Move Assets/Scripts

---
+++ unity/Assets/Maroon/Editor/OptimizeTexture.cs
@@ -0,0 +1,42 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Maroon.Editor
+{
+    public class OptimizeTexture : ScriptableObject
+    {
+        private static readonly TextureImporterPlatformSettings TextureSettingsWebGL = new TextureImporterPlatformSettings()
+        {
+            textureCompression = TextureImporterCompression.Compressed,
+            crunchedCompression = true,
+            compressionQuality = 75,
+            name = "WebGL",
+            overridden = true,
+            maxTextureSize = 512
+        };
+
+        [MenuItem("Assets/Optimize Texture for Platform/WebGL")]
+        static void OptimizeTextureForWebGL()
+        {
+            var count = 0;
+
+            Debug.Log("Start optimizing textures for WebGL");
+
+            foreach (var guid in AssetDatabase.FindAssets("t:texture", null))
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                var textureImporter = AssetImporter.GetAtPath(path) as TextureImporter;
+                if (textureImporter == null) continue;
+
+                Debug.Log($"optimizing: {path}...");
+
+                textureImporter.SetPlatformTextureSettings(TextureSettingsWebGL);
+                AssetDatabase.ImportAsset(path);
+                count++;
+            }
+            Debug.Log(count + " textures optimized");
+        }
+    }
+}
+#endif
