7db4697a0116a4e6cb06d91a5ab6ba69a2b7bb0e
Florian Wohlmuth <wohlmuth.florian@gmx.at>
2024-10-29 13:52:01 +0100

    Swapped out old CoulombsLaw with Remake

---
+++ unity/Assets/Maroon/scenes/experiments/CoulombsLaw/Scripts/PC_ObjectSelectionHandler.cs
@@ -0,0 +1,143 @@
+﻿using GEAR.Localization.Text;
+using Maroon.GlobalEntities;
+using Maroon.Physics.CoordinateSystem;
+using PlatformControls.PC;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Maroon.UI
+{
+    public class PC_ObjectSelectionHandler : MonoBehaviour 
+    {
+        [SerializeField] private GameObject inputXVariable;
+        [SerializeField] private GameObject inputYVariable;
+        [SerializeField] private GameObject inputZVariable;
+
+        [SerializeField] private PC_Slider coulombValueSlider;
+        [SerializeField] private GameObject particlePrefab;
+
+        [SerializeField] private GameObject SourceButtonAddDelete;
+        [SerializeField] private GameObject SourceButtonText;
+
+        [Header("Others")]
+        [SerializeField] private PC_ObjectSelection selectedObject = null;
+        [SerializeField] private ParticleManager _particleManager;
+
+        private PC_InputParser_Float_TMP _xVariableInputParser; 
+        private PC_InputParser_Float_TMP _yVariableInputParser;
+        private PC_InputParser_Float_TMP _zVariableInputParser;
+
+        void Start()
+        {
+            AdaptButtonTextCharge();
+
+            _xVariableInputParser = inputXVariable.GetComponent<PC_InputParser_Float_TMP>();
+            _yVariableInputParser = inputYVariable.GetComponent<PC_InputParser_Float_TMP>();
+            _zVariableInputParser = inputZVariable.GetComponent<PC_InputParser_Float_TMP>();
+
+            _xVariableInputParser?.onValueChangedFloat.AddListener((endVal) =>
+            {
+                if (IsSelectedObjectOfType(PC_ObjectSelection.SelectObjectType.SourceSelect))
+                    MoveToNewPosition(new Vector3(endVal, _yVariableInputParser.GetValue(), _zVariableInputParser.GetValue()));
+            });
+
+            _yVariableInputParser?.onValueChangedFloat.AddListener((endVal) =>
+            {
+                if (IsSelectedObjectOfType(PC_ObjectSelection.SelectObjectType.SourceSelect))
+                    MoveToNewPosition(new Vector3(_xVariableInputParser.GetValue(), endVal , _zVariableInputParser.GetValue()));
+            });
+
+            _yVariableInputParser?.onValueChangedFloat.AddListener((endVal) =>
+            {
+                if (IsSelectedObjectOfType(PC_ObjectSelection.SelectObjectType.SourceSelect))
+                    MoveToNewPosition(new Vector3(_xVariableInputParser.GetValue(), _yVariableInputParser.GetValue(), endVal));
+            });
+
+            coulombValueSlider.onValueChanged.AddListener((newValue) =>
+            {
+                if (!IsSelectedObjectOfType(PC_ObjectSelection.SelectObjectType.SourceSelect)) return;
+           
+                selectedObject.GetComponent<global::Maroon.Physics.Electromagnetism.Charge>().strength = newValue;
+                _particleManager.ChangeColorOfParticle(selectedObject.gameObject, newValue);
+
+            });
+
+            SourceButtonAddDelete.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                if (selectedObject != null && selectedObject.type == PC_ObjectSelection.SelectObjectType.SourceSelect)
+                {
+                    DeleteSelectedCharge();
+                }
+                else
+                {
+                    _particleManager.CreateSource(particlePrefab, CoordSystemHandler.Instance.GetWorldPosition(new Vector3(_xVariableInputParser.GetValue(),
+                        _yVariableInputParser.GetValue(), _zVariableInputParser.GetValue())), coulombValueSlider.value);
+               
+                    AdaptButtonTextCharge();
+                }
+            });
+        }
+
+        private void DeleteSelectedCharge()
+        {
+            var dyingObject = selectedObject.gameObject;
+            SelectObject(null);
+        
+            _particleManager.RemoveSourceFromEField(dyingObject);
+            Destroy(dyingObject);
+            AdaptButtonTextCharge();
+        }
+
+
+        private void AdaptButtonTextCharge()
+        {
+            if (SourceButtonText == null || SourceButtonText.GetComponent<LocalizedTMP>() == null) return;
+            if (!SourceButtonText) return;
+            SourceButtonText.GetComponent<LocalizedTMP>().Key = 
+                selectedObject == null || selectedObject.type != PC_ObjectSelection.SelectObjectType.SourceSelect
+                    ? "Add Charge"
+                    : "Delete Charge";
+        }
+
+        public void SelectObject(PC_ObjectSelection obj)
+        {
+            if (selectedObject == obj) return;
+       
+            selectedObject = obj;
+            if (selectedObject != null)
+            {
+                AdaptButtonTextCharge();
+                UpdatePositionText();
+                UpdateChargeValueText();
+            }
+        }
+
+        private void UpdatePositionText()
+        {
+            var selectedObjectSystemPosition = CoordSystemHandler.Instance.GetSystemPosition(selectedObject.transform.position, Unit.cm);
+
+            _xVariableInputParser.SetValue(selectedObjectSystemPosition.x);
+            _yVariableInputParser.SetValue(selectedObjectSystemPosition.y);
+            _zVariableInputParser.SetValue(selectedObjectSystemPosition.z);
+        }
+
+        private void UpdateChargeValueText()
+        {
+            coulombValueSlider.value = selectedObject.GetComponent<global::Maroon.Physics.Electromagnetism.Charge>().strength;
+        }
+
+        public void DeselectAll()
+        {
+            selectedObject = null;
+            AdaptButtonTextCharge();
+        }
+
+        private void MoveToNewPosition(Vector3 transform)
+        {
+            if (!selectedObject) return;
+            selectedObject.transform.position = CoordSystemHandler.Instance.GetWorldPosition(transform);
+        }
+
+        private bool IsSelectedObjectOfType(PC_ObjectSelection.SelectObjectType type) => selectedObject != null && selectedObject.type == type;
+    }
+}
---
+++ unity/Assets/Maroon/scenes/experiments/CoulombsLaw/Scripts/ParticleManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using Maroon.Physics.Electromagnetism;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    [SerializeField] private GameObject minBoundary;
+    [SerializeField] private GameObject maxBoundary;
+
+    [SerializeField] private EField eField;
+    [SerializeField] private int maxChargeCount = 10;
+    [SerializeField] private bool allowVisualization = true;
+
+    public int GetMaxChargesCount() => maxChargeCount;
+    public bool IsVisualizationAllowed() => allowVisualization;
+   
+    private List<Maroon.Physics.Electromagnetism.Charge> _charges = new List<Maroon.Physics.Electromagnetism.Charge>();
+
+    public GameObject CreateSource(GameObject prefab, Vector3 position, float chargeStrength)
+    {
+        var obj = Instantiate(prefab, position, Quaternion.identity);
+        Debug.Assert(obj != null);
+
+        var movement = obj.GetComponent<PC_DragHandler>();
+        if (!movement) movement = obj.GetComponentInChildren<PC_DragHandler>();
+        if (movement)
+        {
+            movement.SetBoundaries(minBoundary, maxBoundary);
+        }
+
+        var charge = obj.GetComponent<Maroon.Physics.Electromagnetism.Charge>();
+        charge.strength = chargeStrength;
+
+        _charges.Add(charge);
+
+        eField.addProducerToSet(obj);
+        eField.updateProducers();
+
+        ChangeColorOfParticle(obj, chargeStrength);
+
+        return obj;
+    }
+
+    public void RemoveSourceFromEField(GameObject source)
+    {
+        eField.removeProducerFromSet(source);
+        _charges.Remove(source.GetComponent<Maroon.Physics.Electromagnetism.Charge>());
+    }
+
+    public void RemoveAllSourcesFromEField()
+    {
+        foreach (var charge in _charges)
+        {
+            eField.removeProducerFromSet(charge.gameObject);
+            Destroy(charge.gameObject);
+        }
+
+        _charges.Clear();
+    }
+
+    public void ChangeColorOfParticle(GameObject obj, float chargeStrength)
+    {
+        var particleBase = obj.transform.Find("Base").GetComponent<MeshRenderer>();
+        var mat = particleBase.materials;
+
+        if (chargeStrength < 0)
+        {
+            mat[0].color = Color.blue;
+            mat[1].color = Color.white;
+            mat[2].color = mat[0].color;
+        }
+        else if (chargeStrength > 0)
+        {
+            mat[0].color = Color.red;
+            mat[1].color = mat[2].color = Color.white;
+        }
+        else
+        {
+            mat[0].color = Color.green;
+            mat[0].color = mat[1].color = mat[2].color;
+        }
+
+        particleBase.materials = mat;
+    }
+
+    public List<Vector4> GetChargesAsVector()
+    {
+        var list = new List<Vector4>();
+        foreach (var charge in _charges)
+        {
+            var pos = charge.transform.position;
+            list.Add(new Vector4(pos.x, pos.y, pos.z, charge.Strength));
+        }
+
+        return list;
+    }
+
+}
