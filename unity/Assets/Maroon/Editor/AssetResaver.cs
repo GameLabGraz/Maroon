95e889169cb8a6fb1b9e7e73eac927dfb93ae762
Florian Glawogger <73254596+FlorianGlawogger@users.noreply.github.com>
2024-11-14 09:42:11 +0100

    Resave now also scriptableobjects, materials, etc

--- unity/Assets/Maroon/Editor/AssetResaver.cs
+++ unity/Assets/Maroon/Editor/AssetResaver.cs
@@ -2,6 +2,7 @@ using System.IO;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using System;
 
 namespace Maroon.Editor
 {
@@ -11,28 +12,57 @@ namespace Maroon.Editor
     /// </summary>
     public class AssetResaver : EditorWindow
     {
+        private static readonly string[] foldersToSearch = { "Assets" };
+
         [MenuItem("Tools/Resave Assets")]
         public static void ResaveAllAssets()
         {
-            string[] foldersToSearch = new[] { "Assets" };
             int changes = 0;
 
-            // Resave prefabs
-            string[] prefabGuids = AssetDatabase.FindAssets("t:Prefab", foldersToSearch);
-            foreach (string guid in prefabGuids)
+            changes += ResaveAllAssetsOfType<GameObject>("t:Prefab");
+            changes += ResaveAllAssetsOfType<Material>("t:Material");
+            changes += ResaveAllAssetsOfType<Shader>("t:Shader");
+            changes += ResaveAllAssetsOfType<ScriptableObject>("t:ScriptableObject");
+            changes += ResaveAllAssetsOfType<Texture>("t:Texture");
+            changes += ResaveAllAssetsOfType<AnimationClip>("t:AnimationClip");
+            changes += ResaveAllScenes();
+
+            AssetDatabase.Refresh();
+            Debug.Log($"Finished resaving assets. Made {changes} change(s).");
+        }
+
+        /// <summary>
+        /// Resaves all assets of a certain type
+        /// </summary>
+        /// <typeparam name="T">The type of asset to resave, e.g. Materials</typeparam>
+        /// <param name="filter">File filter, e.g. t:Material</param>
+        /// <returns>The number of assets resaved</returns>
+        private static int ResaveAllAssetsOfType<T>(string filter) where T : UnityEngine.Object
+        {
+            int changes = 0;
+            string[] guids = AssetDatabase.FindAssets(filter, foldersToSearch);
+            foreach (string guid in guids)
             {
-                string prefabPath = AssetDatabase.GUIDToAssetPath(guid);
-                GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
-                if (prefab != null)
+                string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                T asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+                if (asset != null)
                 {
-                    EditorUtility.SetDirty(prefab);
+                    EditorUtility.SetDirty(asset);
                     AssetDatabase.SaveAssets();
                     changes++;
-                    Debug.Log("Resaved Prefab: " + prefabPath);
                 }
             }
+            Debug.Log($"Resaved {changes} assets of type {typeof(T).Name}.");
+            return changes;
+        }
 
-            // Resave Scenes
+        /// <summary>
+        /// Resaves all scenes
+        /// </summary>
+        /// <returns>The number of scenes resaved</returns>
+        private static int ResaveAllScenes()
+        {
+            int changes = 0;
             string[] sceneGuids = AssetDatabase.FindAssets("t:Scene", foldersToSearch);
             foreach (string guid in sceneGuids)
             {
@@ -42,12 +72,11 @@ namespace Maroon.Editor
                     EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Additive);
                     EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
                     changes++;
-                    Debug.Log("Resaved Scene: " + scenePath);
                 }
             }
 
-            AssetDatabase.Refresh();
-            Debug.Log($"Finished resaving assets. Made {changes} change(s).");
+            Debug.Log($"Resaved {changes} scenes.");
+            return changes;
         }
     }
 }
