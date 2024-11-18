784cfa01139f04c99e5a0ebe314cd5e6e09dd88e
Florian Wohlmuth <wohlmuth.florian@gmx.at>
2024-11-18 11:51:09 +0100

    Added type to Json files

---
+++ unity/Assets/Maroon/reusableScripts/Config/JsonFileLoader.cs
@@ -0,0 +1,94 @@
+
+using UnityEngine;
+using UnityEngine.Events;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+using UnityEngine.Networking;
+using System;
+using System.Collections;
+using Newtonsoft.Json;
+#endif
+
+namespace Maroon.Config
+{
+    public class JsonFileLoader : MonoBehaviour
+    {
+        public UnityEvent<List<TextAsset>> OnFilesInitialized = new UnityEvent<List<TextAsset>>();
+
+        private void Start()
+        {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            StartCoroutine(LoadAllConfigs());
+#else
+            string basePath = Path.Combine(Application.streamingAssetsPath, "Config", "3DMotionSimulation");
+            string[] txtFiles = Directory.GetFiles(basePath, "*.json");
+            List<TextAsset> assets = new List<TextAsset>();
+
+            foreach (string file in txtFiles)
+            {
+                string jsonText = File.ReadAllText(file);
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                
+                TextAsset textAsset = new TextAsset(jsonText);
+                textAsset.name = fileName;
+
+                assets.Add(textAsset);
+            }
+
+            OnFilesInitialized.Invoke(assets);
+#endif
+        } 
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+        private IEnumerator LoadAllConfigs()
+        {
+            string baseDomain = new Uri(Application.absoluteURL).ToString();
+            if (baseDomain.Contains("?")) baseDomain = baseDomain.Substring(0, baseDomain.IndexOf('?'));
+            string basePath = $"{baseDomain}/StreamingAssets/Config/3DMotionSimulation/";
+            string configListUrl = $"{baseDomain}/configs.php";
+            
+            List<string> httpFiles = new List<string>();
+            UnityWebRequest uwr = UnityWebRequest.Get(configListUrl);
+            
+            yield return uwr.SendWebRequest();
+
+            var jsonFile = uwr.downloadHandler.text;
+            var parseJSON = JsonConvert.DeserializeObject<List<string>>(jsonFile);
+
+            for(int i = 0; i < parseJSON.Count; i++) {
+                httpFiles.Add(basePath + parseJSON[i]);
+            }
+
+            for(int i = 0; i < httpFiles.Count; i++) {
+                UnityWebRequest webReq = UnityWebRequest.Get(httpFiles[i]);
+                yield return webReq.SendWebRequest();
+
+                var jsonText = webReq.downloadHandler.text;
+                string fileName = Path.GetFileNameWithoutExtension(httpFiles[i]);
+                
+                TextAsset textAsset = new TextAsset(jsonText);
+                textAsset.name = fileName;
+
+                assets.Add(textAsset);
+            }
+
+            OnFilesInitialized.Invoke(assets);
+            
+            /*if(Maroon.GlobalEntities.BootstrappingManager.Instance.UrlParameters.TryGetValue(WebGlUrlParameter.Config, out string config))
+            {
+                if (!ChangeConfig(config))
+                {
+                    ChangeConfig("Default");
+                }
+            }
+            else
+            {
+                ChangeConfig("Default");
+            }*/
+        }
+#endif
+    }
+}
