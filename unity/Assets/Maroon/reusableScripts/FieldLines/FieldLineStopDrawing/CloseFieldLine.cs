05218cbff35d6227786191cde3ef067fe8ecf2e0
Florian Glawogger <73254596+FlorianGlawogger@users.noreply.github.com>
2025-01-30 18:01:46 +0100

    CloseFieldLine stop drawing check is now less strict

--- unity/Assets/Maroon/reusableScripts/FieldLines/FieldLineStopDrawing/CloseFieldLine.cs
+++ unity/Assets/Maroon/reusableScripts/FieldLines/FieldLineStopDrawing/CloseFieldLine.cs
@@ -4,26 +4,24 @@ using UnityEngine;
 [RequireComponent(typeof(FieldLine))]
 public class CloseFieldLine : MonoBehaviour
 {
-    [SerializeField]
-    private bool useCoilHack = false;
+    private FieldLine fieldLine;
 
     private void Awake()
     {
-        var fieldLine = GetComponent<FieldLine>();
+        fieldLine = GetComponent<FieldLine>();
         if (fieldLine)
             fieldLine.stopDrawingCheck = StopDrawing;
     }
 
-    private bool StopDrawing(Vector3 position)
+    private bool StopDrawing(Vector3 lineSegmentPosition)
     {
         var emObj = transform.parent.gameObject;
 
         var dist = transform.position;
 
-        if (useCoilHack) // hack for coil
-            dist -= 1.5f * new Vector3(Mathf.Abs(emObj.transform.up.x), Mathf.Abs(emObj.transform.up.y), Mathf.Abs(emObj.transform.up.z));
-
-        return Vector3.Distance(position, dist) <= 0.053f ||
-               Vector3.Distance(position, transform.position) <= 0.027f;
+        Vector3 fieldLineOffsetStart = fieldLine.transform.TransformPoint(Vector3.zero - fieldLine.originOffset);
+        float lineDistanceToStart = Vector3.Distance(lineSegmentPosition, fieldLineOffsetStart);
+        bool closeToStart = lineDistanceToStart <= (fieldLine.minLineSegmentLength * 2);
+        return closeToStart;
     }
 }
