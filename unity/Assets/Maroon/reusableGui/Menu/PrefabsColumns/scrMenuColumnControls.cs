a324b38dfdb40f7500c1f6a6b7cf1fb7599fa580
Florian Glawogger <73254596+FlorianGlawogger@users.noreply.github.com>
2025-02-05 14:26:06 +0100

    Add scrMenuColumnControls

---
+++ unity/Assets/Maroon/reusableGui/Menu/PrefabsColumns/scrMenuColumnControls.cs
@@ -0,0 +1,26 @@
+﻿using Maroon.GlobalEntities;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class scrMenuColumnControls : MonoBehaviour
+{
+    [Header("Buttons/Sliders")]
+    [SerializeField] private Slider _mouseSensitivitySlider;
+
+
+    private void Start()
+    {
+        if (_mouseSensitivitySlider)
+        {
+            // TODO: _mouseSensitivitySlider.value = SoundManager.Instance.MusicVolume;
+            _mouseSensitivitySlider.onValueChanged.AddListener(this.OnChangeMouseSensitivitySlider);
+        }
+    }
+
+    public void OnChangeMouseSensitivitySlider(float value)
+    {
+        // TODO
+        // e.g. SoundManager.Instance.MusicVolume = value;
+    }
+}
