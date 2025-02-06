b15de6646fd50ca5c75dbe9b8e350eeb16acf0d5
Florian Glawogger <73254596+FlorianGlawogger@users.noreply.github.com>
2025-02-06 13:11:20 +0100

    Adjust SettingsManager code and add to global entities prefab

--- unity/Assets/Maroon/GlobalEntities/SettingsManager/SettingsManager.cs
+++ unity/Assets/Maroon/GlobalEntities/SettingsManager/SettingsManager.cs
@@ -1,112 +1,19 @@
 ﻿using UnityEngine;
 
-namespace Maroon.GlobalEntities
+namespace Maroon.GlobalEntities.SettingsManager
 {
     /// <summary>
-    ///     Handles tasks related to sound in Maroon. It keeps settings about audio levels, can play audio effects that
-    ///     are common in Maroon (e.g. alerts or clicks) and plays background music.
+    ///     Responsible for storing settings persistently on the Computer
+    ///     (i.e. Saves audio volume levels, selected language, and mouse sensitivity)
     /// </summary>
-    public class SoundManager : MonoBehaviour, GlobalEntity
+    public class SettingsManager : MonoBehaviour, GlobalEntity
     {
-        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        // Fields
-
-        // -------------------------------------------------------------------------------------------------------------
-        // Singleton
-
-        private static SoundManager _instance = null;
-
-        // -------------------------------------------------------------------------------------------------------------
-        // Sources
-
-        // For sound effects
-        [SerializeField] private AudioSource _soundEffectSource = null;
-
-        // For music
-        [SerializeField] private AudioSource _musicSource = null;
-
-        // -------------------------------------------------------------------------------------------------------------
-        // Common Sounds
-
+        private static SettingsManager _instance = null;
         /// <summary>
-        ///     Sound to be played for clicks or default actions in the user interface. E.g. clicking on a button.
+        ///     The SettingsManager instance
         /// </summary>
-        [SerializeField] private AudioClip _uiPrimary = null;
-
-        /// <summary>
-        ///     Sound to be played for information actions. E.g. an alert about a system state change.
-        /// </summary>
-        [SerializeField] private AudioClip _uiInfo = null;
-
-
-        /// <summary>
-        ///     Sound to be played for successful actions. E.g. an alert about a succesful connection to a server.
-        /// </summary>
-        [SerializeField] private AudioClip _uiSuccess = null;
-
-        /// <summary>
-        ///     Sound to be played for actions. E.g. an alert about a disconnect from a server.
-        /// </summary>
-        [SerializeField] private AudioClip _uiWarning = null;
-
-        /// <summary>
-        ///     Sound to be played for failed actions. E.g. an alert about a failed attempt to load a resource.
-        /// </summary>
-        [SerializeField] private AudioClip _uiDanger = null;
-
-        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        // Properties, Getters and Setters
-
-        // -------------------------------------------------------------------------------------------------------------
-        // Singleton
-
-        /// <summary>
-        ///     The GameManager instance
-        /// </summary>
-        public static SoundManager Instance => SoundManager._instance;
+        public static SettingsManager Instance => SettingsManager._instance;
         MonoBehaviour GlobalEntity.Instance => Instance;
-
-        // -------------------------------------------------------------------------------------------------------------
-        // Audio sources
-        
-        /// <summary>
-        ///     Getter for Sound Effect AudioSource component
-        /// </summary>
-        public AudioSource soundEffectSource => _soundEffectSource;
-
-        // -------------------------------------------------------------------------------------------------------------
-        // Volume
-
-        /// <summary>
-        ///     The volume of the music audio source. A float from 0.0 to 1.0.
-        /// </summary>
-        public float MusicVolume
-        {
-            get { return _musicSource ? _musicSource.volume : 0f; }
-            set { 
-                if(_musicSource)
-                    _musicSource.volume = Mathf.Clamp(value, 0f, 1f); 
-            }
-        }
-
-        /// <summary>
-        ///     The volume of the sound effect audio source. A float from 0.0 to 1.0.
-        /// </summary>
-        public float SoundEffectVolume
-        {
-            get { return _soundEffectSource ? _soundEffectSource.volume : 0f; }
-            set
-            {
-                if(_soundEffectSource)
-                    _soundEffectSource.volume = Mathf.Clamp(value, 0f, 1f);
-            }
-        }
-
-        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        // Methods
-
-        // -------------------------------------------------------------------------------------------------------------
-        // Initialization
 
         /// <summary>
         ///     Called by Unity. Initializes singleton instance and DontDestroyOnLoad (stays active on new scene load).
@@ -114,11 +21,11 @@ namespace Maroon.GlobalEntities
         private void Awake()
         {
             // Singleton
-            if(SoundManager._instance == null)
+            if(SettingsManager._instance == null)
             {
-                SoundManager._instance = this;
+                SettingsManager._instance = this;
             }
-            else if(SoundManager._instance != this)
+            else if(SettingsManager._instance != this)
             {
                 DestroyImmediate(this.gameObject);
                 return;
@@ -128,16 +35,5 @@ namespace Maroon.GlobalEntities
             this.transform.parent = null;
             DontDestroyOnLoad(this.gameObject);
         }
-
-        // -------------------------------------------------------------------------------------------------------------
-        // Play Effects
-
-        public void PlaySoundEffect(AudioClip clip)
-        {
-            _soundEffectSource.clip = clip;
-            _soundEffectSource.Play();
-        }
-        
-        //TODO: Methods for default sounds
     }
 }
