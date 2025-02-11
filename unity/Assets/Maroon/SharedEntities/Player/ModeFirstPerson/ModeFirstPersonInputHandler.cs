becd0ea9c35aad7b7d81b71a59c3868b2e5605a3
Florian Glawogger <73254596+FlorianGlawogger@users.noreply.github.com>
2025-02-11 13:19:35 +0100

    Maroon-389 Add jump input buffering (#572)

--- unity/Assets/Maroon/SharedEntities/Player/ModeFirstPerson/ModeFirstPersonInputHandler.cs
+++ unity/Assets/Maroon/SharedEntities/Player/ModeFirstPerson/ModeFirstPersonInputHandler.cs
@@ -16,6 +16,15 @@ public class ModeFirstPersonInputHandler : MonoBehaviour
     ModeFirstPerson m_PlayerCharacterController;
     bool m_FireInputWasHeld;
 
+    /// <summary>
+    /// The last time the jump input was pressed
+    /// </summary>
+    private float lastTimeJumpPressed = float.MinValue;
+    /// <summary>
+    /// The time for how early you can press Jump in the air and still jump when touching the ground. In seconds
+    /// </summary>
+    private float jumpInputBufferingTime = 0.15f;
+
     private void Start()
     {
         m_PlayerCharacterController = GetComponent<ModeFirstPerson>();
@@ -29,6 +38,19 @@ public class ModeFirstPersonInputHandler : MonoBehaviour
     private void LateUpdate()
     {
         m_FireInputWasHeld = GetFireInputHeld();
+    }
+
+    private void Update()
+    {
+        if (!CanProcessInput())
+        {
+            return;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            lastTimeJumpPressed = Time.time;
+        }
     }
 
     public bool CanProcessInput()
@@ -66,7 +88,7 @@ public class ModeFirstPersonInputHandler : MonoBehaviour
     {
         if (CanProcessInput())
         {
-            return Input.GetButtonDown("Jump");
+            return (lastTimeJumpPressed + jumpInputBufferingTime) > Time.time;
         }
 
         return false;
