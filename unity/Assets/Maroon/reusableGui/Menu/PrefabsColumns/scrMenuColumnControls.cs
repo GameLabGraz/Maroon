d989193a6ae5592ab9466bc76691f944f174466a
Florian Glawogger <73254596+FlorianGlawogger@users.noreply.github.com>
2025-02-20 09:28:28 +0100

    Mouse Sensitivity silder now uses logarithmic range

--- unity/Assets/Maroon/reusableGui/Menu/PrefabsColumns/scrMenuColumnControls.cs
+++ unity/Assets/Maroon/reusableGui/Menu/PrefabsColumns/scrMenuColumnControls.cs
@@ -5,20 +5,33 @@ using UnityEngine.UI;
 
 public class scrMenuColumnControls : MonoBehaviour
 {
+    const float MIN_MOUSE_SENSITIVITY = 20;
+    const float MAX_MOUSE_SENSITIVITY = 1000;
+
     [Header("Buttons/Sliders")]
     [SerializeField] private Slider _mouseSensitivitySlider;
 
+    
     private void Start()
     {
         if (_mouseSensitivitySlider)
         {
-            _mouseSensitivitySlider.value = ControlsManager.Instance.MouseSensitivity;
+            // Set Slider range from 0 to 1
+            _mouseSensitivitySlider.minValue = 0f;
+            _mouseSensitivitySlider.maxValue = 1f;
+            // Convert mouse sensitivity to value between 0 and 1 for the logarithmic slider
+            float mouseSensitivity = ControlsManager.Instance.MouseSensitivity;
+            float sliderValue01 = Mathf.Log(mouseSensitivity / MIN_MOUSE_SENSITIVITY) / Mathf.Log(MAX_MOUSE_SENSITIVITY / MIN_MOUSE_SENSITIVITY);
+            _mouseSensitivitySlider.value = sliderValue01;
+
             _mouseSensitivitySlider.onValueChanged.AddListener(this.OnChangeMouseSensitivitySlider);
         }
     }
 
-    public void OnChangeMouseSensitivitySlider(float value)
+    public void OnChangeMouseSensitivitySlider(float sliderValue01)
     {
-        ControlsManager.Instance.MouseSensitivity = value;
+        // Convert slider value (betwenn 0 and 1) logarithmically to a mouse sensitvity value between min and max
+        float mouseSensitivity = Mathf.Pow((MAX_MOUSE_SENSITIVITY / MIN_MOUSE_SENSITIVITY), sliderValue01) * MIN_MOUSE_SENSITIVITY;
+        ControlsManager.Instance.MouseSensitivity = mouseSensitivity;
     }
 }
